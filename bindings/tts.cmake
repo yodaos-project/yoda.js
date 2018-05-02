044fac94054335b2ac4cd9e8dc6f4dc4b49e5fb1
Yorkie Liu <yorkiefixer@gmail.com>
2018-05-03 04:07:16 +0800

    fix binding path

--- bindings/tts.cmake
+++ bindings/tts.cmake
@@ -1,9 +1,11 @@
 project(libtts C)
-add_library(libtts MODULE tts.c)
+set(ROKIDOS_BINDINGS_DIR ${CMAKE_SOURCE_DIR}/bindings)
+
+add_library(libtts MODULE ${ROKIDOS_BINDINGS_DIR}/tts.c)
 set_target_properties(libtts PROPERTIES
   PREFIX ""
   SUFFIX ".node"
-  OUTPUT_NAME "binding"
+  OUTPUT_NAME "libtts"
   LINK_FLAGS "-undefined dynamic_lookup"
 )
 target_include_directories(libtts PUBLIC ${SHADOW_NODE_HEADER_PATH})
