d953be8502bb31eaf6a8beab2c6dd78878251d1b
Yorkie Liu <yorkiefixer@gmail.com>
2018-05-03 04:33:30 +0800

    update tts done

--- bindings/tts.c
+++ bindings/tts.c
@@ -3,17 +3,72 @@
 #include <shadow-node/iotjs.h>
 #include <shadow-node/iotjs_def.h>
 #include <shadow-node/iotjs_binding.h>
+#include <tts/tts_client.h>
 
-JS_FUNCTION(Cal) {
-  for (int i = 0; i < 100000; i++) {
-    // just no
+typedef struct {
+  iotjs_jobjectwrap_t jobjectwrap;
+  struct tts_callback listeners;
+} IOTJS_VALIDATED_STRUCT(iotjs_tts_t);
+
+void onttsstart(int id, void* data) {
+  printf("onttsstart\n");
+}
+
+void onttscancel(int id, void* data) {
+  printf("onttscancel\n");
+}
+
+void onttsdone(int id, void* data) {
+  printf("onttsdone\n");
+}
+
+void onttserror(int id, int err, void* data) {
+  printf("onttserror\n");
+}
+
+static iotjs_tts_t* iotjs_ttswrap_create(const jerry_value_t value) {
+  iotjs_ttswrap_t* tlswrap = IOTJS_ALLOC(iotjs_ttswrap_t);
+  IOTJS_VALIDATED_STRUCT_CONSTRUCTOR(iotjs_ttswrap_t, tts);
+  iotjs_jobjectwrap_initialize(&_this->jobjectwrap, 
+                               value,
+                               &this_module_native_info);
+  _this->listeners = {
+    onttsstart,
+    onttscancel,
+    onttsdone,
+    onttserror,
+  };
+  int r = tts_init();
+  if (r != -1) {
+    tts_set(&_this->listeners);
   }
-  return jerry_create_boolean(true);
+  return tts;
 }
 
-void init(jerry_value_t exports) {
-  iotjs_jval_set_property_number(exports, "foobar", 10);
-  iotjs_jval_set_method(exports, "cal", Cal);
+static void iotjs_ttswrap_destroy(iotjs_ttswrap_t* tts) {
+  IOTJS_VALIDATED_STRUCT_DESTRUCTOR(iotjs_tlswrap_t, tts);
+  iotjs_jobjectwrap_destroy(&_this->jobjectwrap);
+  IOTJS_RELEASE(tts);
 }
 
-NODE_MODULE(tts, init)
+S_FUNCTION(TtsConstructor) {
+  DJS_CHECK_THIS();
+  iotjs_ttswrap_create(JS_GET_THIS());
+  return jerry_create_undefined();
+}
+
+void InitTts(jerry_value_t exports) {
+  jerry_value_t tts = jerry_create_object();
+  jerry_value_t ttsConstructor =
+      jerry_create_external_function(TtsConstructor);
+  iotjs_jval_set_property_jval(tls, "TtsWrap", ttsConstructor);
+
+  jerry_value_t proto = jerry_create_object();
+  iotjs_jval_set_property_jval(ttsConstructor, "prototype", proto);
+
+  jerry_release_value(proto);
+  jerry_release_value(ttsConstructor);
+  return tts;
+}
+
+NODE_MODULE(libtts, InitTts)
