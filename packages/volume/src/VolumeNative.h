9c08a59b148ae36749890db59e06c4840d459d42
Yazhong Liu <yazhong.liu@rokid.com>
2018-07-13 20:19:06 +0800

    add volume apis
    Change-Id: I22d46beeb8ef6ebe0c17a8f138648633f1a05ad5

---
+++ packages/volume/src/VolumeNative.h
@@ -0,0 +1,18 @@
+#ifndef VOL_NATIVE_H
+#define VOL_NATIVE_H
+
+#include <stdio.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif /* __cplusplus */
+
+#include <iotjs.h>
+#include <iotjs_def.h>
+#include <iotjs_binding.h>
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+#endif // TTS_NATIVE_H
