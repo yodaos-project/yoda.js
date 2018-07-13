45ebac0e3e6d0a5d6acc376b52da6c9f36d0dc01
Yazhong Liu <yazhong.liu@rokid.com>
2018-07-13 19:33:46 +0800

    add tts
    Change-Id: Ic95cd06f99cb4f5147517fc540c2808675e399d3

--- packages/tts/index.js
+++ packages/tts/index.js
@@ -1,3 +1,17 @@
 'use strict';
 
-var ttswrap = require('./tts.node');
+var TtsWrap = require('./tts.node').TtsWrap;
+function createHandle(options) {
+  var handle = new TtsWrap();
+  handle.prepare(
+    options.host || 'apigwws.open.rokid.com', 443, '/api',
+    options.key,
+    options.deviceTypeId,
+    options.deviceId,
+    options.secret,
+    options.declaimer || 'zh');
+  return handle;
+}
+
+exports.createHandle = createHandle;
+
