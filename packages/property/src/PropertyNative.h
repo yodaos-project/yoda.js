e3db4585eac33e73ddf42165537852df05cef16b
Yazhong Liu <yazhong.liu@rokid.com>
2018-07-15 20:29:44 +0800

    add property
    Change-Id: Idfa0aab6e3914de9394e02ec1a03c3b28f992b8c

---
+++ packages/property/src/PropertyNative.h
@@ -0,0 +1,18 @@
+#ifndef PROPERTY_NATIVE_H
+#define PROPERTY_NATIVE_H
+
+#include <stdio.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif /* __cplusplus */
+
+#include <iotjs.h>
+#include <iotjs_def.h>
+#include <iotjs_binding.h>
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+#endif // PROPERTY_NATIVE_H
