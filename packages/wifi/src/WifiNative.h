273de5c85d185cbe930fb1090bed25936c73a950
Yazhong Liu <yazhong.liu@rokid.com>
2018-07-15 19:11:23 +0800

    fix names of wifi
    Change-Id: I83193b6d48fe75331f1e525b95ed9eeb2998b156

---
+++ packages/wifi/src/WifiNative.h
@@ -0,0 +1,18 @@
+#ifndef WIFI_NATIVE_H
+#define WIFI_NATIVE_H
+
+#include <stdio.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif /* __cplusplus */
+
+#include <iotjs.h>
+#include <iotjs_def.h>
+#include <iotjs_binding.h>
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+#endif // WIFI_NATIVE_H
