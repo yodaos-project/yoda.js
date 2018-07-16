0b3b2dba8392473595b753fe8d13de0e5102f9f9
Yazhong Liu <yazhong.liu@rokid.com>
2018-07-16 13:20:25 +0800

    fix light
    Change-Id: Ie1400f57373c48063d61ac7b070baae75e7962aa

--- packages/light/index.js
+++ packages/light/index.js
@@ -1,41 +1,8 @@
 'use strict';
 
 /**
- * @module volume
+ * @module light
  */
 
-var native = require('./volume.node');
-
-/**
- * @method set
- * @param {String} stream - <optional> the stream type, tts/audio/alarm.
- * @param {Number} vol - the volume to set
- */
-exports.set = function(stream, vol) {
-  if (arguments.length === 1 && typeof stream === 'number') {
-    vol = stream;
-    stream = null;
-  }
-  if (!vol) {
-    throw new TypeError('vol is required');
-  }
-
-  if (stream) {
-    native.setStreamVolume(stream, vol);
-  } else {
-    native.setVolume(vol);
-  }
-};
-
-/**
- * @method get
- * @param {String} stream - <optional> the stream type, tts/audio/alarm
- */
-exports.get = function(stream) {
-  if (stream) {
-    return native.getStreamVolume(stream);
-  } else {
-    return native.getVolume();
-  }
-};
+module.exports = require('./light.node');
 
