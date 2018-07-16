0d783ad3cad8eef6e7c7e9a8294eb6155fc5665b
Yazhong Liu <yazhong.liu@rokid.com>
2018-07-16 13:18:19 +0800

    refine the structure
    Change-Id: I3b8640bd941d916eabbc6a7c4bc2f885a50af054

--- packages/multimedia/index.js
+++ packages/multimedia/index.js
@@ -1,17 +1,20 @@
 'use strict';
 
-var TtsWrap = require('./tts.node').TtsWrap;
-function createHandle(options) {
-  var handle = new TtsWrap();
-  handle.prepare(
-    options.host || 'apigwws.open.rokid.com', 443, '/api',
-    options.key,
-    options.deviceTypeId,
-    options.deviceId,
-    options.secret,
-    options.declaimer || 'zh');
-  return handle;
+/**
+ * @module multimedia
+ */
+
+var native = require('./multimedia.node');
+
+/**
+ * @class MediaPlayer
+ */
+function MediaPlayer() {
+  // TODO
 }
 
-exports.createHandle = createHandle;
+/**
+ * @module multimedia.soundplayer
+ */
+exports.soundplayer = require('./soundplayer');
 
