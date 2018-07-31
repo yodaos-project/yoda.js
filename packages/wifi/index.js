a61f16c80fdd227802c23b20720e946708eb1889
Yazhong Liu <yazhong.liu@rokid.com>
2018-07-31 21:04:59 +0800

    wifi: add enum for status match and docs
    Change-Id: I30c5cc4a79bccc33c1227f1cceaebd41c2d862bd

--- packages/wifi/index.js
+++ packages/wifi/index.js
@@ -9,26 +9,71 @@ var native = require('./wifi.node');
 module.exports = {
   /**
    * @memberof wifi
+   * @var WIFI_INIVATE {Number} - wifi is invalid
+   */
+  WIFI_INIVATE: 0,
+  /**
+   * @memberof wifi
+   * @var WIFI_SCANING {Number} - wifi is scanning, just waiting
+   */
+  WIFI_SCANING: 1,
+  /**
+   * @memberof wifi
+   * @var WIFI_CONNECTED {Number} - wifi is connected.
+   */
+  WIFI_CONNECTED: 2,
+  /**
+   * @memberof wifi
+   * @var WIFI_UNCONNECTED {Number} - the wifi is disconnected.
+   */
+  WIFI_UNCONNECTED: 3,
+  /**
+   * @memberof wifi
+   * @var NETSERVER_CONNECTED {Number} - the networking is connected.
+   */
+  NETSERVER_CONNECTED: 4,
+  /**
+   * @memberof wifi
+   * @var NETSERVER_UNCONNECTED {Number} - the networking is disconnected.
+   */
+  NETSERVER_UNCONNECTED: 5,
+  /**
+   * Connect a WI-FI via ssid, psk and method.
+   * @memberof wifi
+   * @function joinNetwork
+   * @param {String} ssid
+   * @param {String} psk
    */
   joinNetwork: native.joinNetwork,
   /**
+   * Get current wifi state.
    * @memberof wifi
+   * @function getWifiState
    */
   getWifiState: native.getWifiState,
   /**
+   * Get current networking state.
    * @memberof wifi
+   * @function getNetworkState
    */
   getNetworkState: native.getNetworkState,
   /**
+   * Disable the WI-FI.
    * @memberof wifi
+   * @function disableAll
    */
   disableAll: native.disableAll,
   /**
+   * Reset the DNS resolver, commonly it needs a call when network is connected.
    * @memberof wifi
+   * @function resetDns
    */
   resetDns: native.resetDns,
   /**
+   * Save the current WI-FI config in local file.
    * @memberof wifi
+   * @function save
    */
   save: native.save,
 };
+
