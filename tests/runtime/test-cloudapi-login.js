f5e30f6f62f2e7ba01a1b1e58f6e4e865e156047
Yorkie Liu <yorkiefixer@gmail.com>
2018-08-10 18:58:50 +0800

    add test for login api
    Change-Id: I017e6d1643cf9eb58a69c8d298264888fb2bb937

--- tests/runtime/test-cloudapi-login.js
+++ tests/runtime/test-cloudapi-login.js
@@ -1,13 +1,16 @@
 'use strict';
 
 var test = require('tape');
+var prop = require('property');
 var login = require('/usr/lib/yoda/runtime/cloudapi/login');
 
 test('login', function(t) {
-  var profile = light.getProfile();
-  t.equal(typeof profile.leds, 'number');
-  t.equal(typeof profile.format, 'number');
-  t.equal(typeof profile.maximumFps, 'number');
-  t.equal(typeof profile.micAngle, 'number');
-  t.end();
+  t.plan(4);
+  login().then((data) => {
+    t.equal(data.device_id, prop.get('ro.boot.serialno'), 'the login device id is checked');
+    t.equal(typeof data.device_type_id, 'string', 'the login device type id is checked');
+    t.equal(typeof data.key, 'string', 'the login key is checked');
+    t.equal(typeof data.secret, 'string', 'the login secret is checked');
+    t.end();
+  });
 });
