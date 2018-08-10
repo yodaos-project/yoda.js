20fff63232d8a655bf36316460a7d6d078e9aefe
Yorkie Liu <yorkiefixer@gmail.com>
2018-08-10 13:24:16 +0800

    add more package tests
    Change-Id: Ibfb602f868074d9e98308b6ee10d804cfa1e592d

---
+++ tests/packages/test-light.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var test = require('tape');
+var light = require('light');
+
+test('light get profile', function(t) {
+  var profile = light.getProfile();
+  t.equal(typeof profile.leds, 'number');
+  t.equal(typeof profile.format, 'number');
+  t.equal(typeof profile.maximumFps, 'number');
+  t.equal(typeof profile.micAngle, 'number');
+  t.end();
+});
+
+test('light simple render', function(t) {
+  var profile = light.getProfile();
+  light.enable();
+
+  var buf = new Buffer(profile.leds * profile.format);
+  buf.fill(233);
+  light.write(buf);
+  setTimeout(() => {
+    buf.fill(0);
+    light.write(buf);
+    t.end();
+  }, 1500);
+});
