8f76ff08cf638545649e8dfcac9f2e627eadb31f
Yorkie Liu <yorkiefixer@gmail.com>
2018-08-12 14:46:41 +0800

    add system app
    Change-Id: I443c8b911c5cb7173164a8ce008deea4a6b790c9

---
+++ apps/system/app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var wifi = require('wifi');
+
+module.exports = function(app) {
+  app.on('ready', function () {
+    console.log(this.getAppId() + ' app ready');
+  });
+  app.on('onrequest', function (nlp, action) {
+    if (nlp.intent === 'disconnect_network') {
+      wifi.disableAll();
+      app.exit();
+    } else if (nlp.intent === 'sleep' ||
+      nlp.intent === 'dormancy' || 
+      nlp.intent === 'usersleep' ||
+      nlp.intent === 'ROKID.SYSTEM.EXIT') {
+      app.exit();
+    } else {
+      app.tts.say('什么设置', function() {
+        app.exit();
+      });
+    }
+  });
+};
