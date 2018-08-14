95f02a4d08ffa4088718d9745214356d99d8e9fd
Yazhong Liu <yazhong.liu@rokid.com>
2018-08-14 18:28:35 +0800

    packages: add input event module
    Change-Id: I0ce99147b6bb61f2f84ee81ac35cbd1185555e88

--- packages/input/src/InputNative.cc
+++ packages/input/src/InputNative.cc
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #include "InputNative.h"
 
 #ifdef HAS_TOUCHPAD
@@ -5,6 +6,10 @@
 #else
 #define IOTJS_INPUT_HAS_TOUCH false
 #endif
+
+static JNativeInfoType this_module_native_info = {
+  .free_cb = (jerry_object_native_free_callback_t)iotjs_input_destroy
+};
 
 class InputInitializer {
  public:
@@ -26,7 +31,7 @@ class InputInitializer {
       InputInitializer::DoStart, InputInitializer::AfterStart);
   }
   int stop() {
-    return uv_cancel((uv_req_t*)req);
+    return uv_cancel((uv_req_t*)&req);
   }
 
  public:
@@ -63,93 +68,115 @@ class InputInitializer {
   int timeout_select;
   int timeout_dbclick;
   int timeout_slide;
-}
+};
 
 class InputKeyEvent {
  public:
   InputEventHandler* event_handler;
   struct keyevent data;
-}
+};
 
 class InputGestureEvent {
  public:
   InputEventHandler* event_handler;
   struct gesture data;
+};
+
+InputEventHandler::InputEventHandler() {
+  // TODO
 }
 
-class InputEventHandler {
- public:
-  InputEventHandler() {}
-  InputEventHandler(iotjs_input_t* inputwrap_) {
-    inputwrap = inputwrap_;
-    keyevent_ = { 0 };
-    gesture_ = { 0 };
-    req.data = this;
-  }
-  ~InputEventHandler() {}
- public:
+InputEventHandler::InputEventHandler(iotjs_input_t* inputwrap_) {
+  inputwrap = inputwrap_;
+  keyevent_ = { 0 };
+  gesture_ = { 0 };
+  req.data = this;
+}
 
-  int start() {
-    return uv_queue_work(uv_default_loop(), &req,
-                         InputEventHandler::DoStart,
-                         InputEventHandler::AfterStart);
-  }
-  int stop() {
-    return uv_cancel((uv_req_t*)req);
-  }
+InputEventHandler::~InputEventHandler() {
+  // TODO
+}
 
- public:
-  static void DoStart(uv_work_t* req) {
-    InputEventHandler* handler = (InputEventHandler*)req->data;
-    while (true) {
-      daemon_start_listener(&handler->keyevent_,
-                            &handler->gesture_);
-      // Send InputKeyEvent
-      if (handler->keyevent_.new_action) {
-        uv_async_t* async = new uv_async_t;
-        InputKeyEvent* event = new InputKeyEvent();
-        event->data.new_action = handler->keyevent_.new_action;
-        event->data.value = handler->keyevent_.value;
-        event->data.action = handler->keyevent_.action;
-        event->data.key_code = handler->keyevent_.key_code;
-        event->data.key_time = handler->keyevent_.key_time;
-        async->data = (void*)event;
-        uv_async_init(uv_default_loop(), async,
-                      InputEventHandler::OnKeyEvent);
-        uv_async_send(async);
-      }
-      // Send InputGestureEvent
-      if (handler->gesture_.new_action) {
-        uv_async_t* async = new uv_async_t;
-        InputGestureEvent* event = new InputGestureEvent();
-        event->data.new_action = handler->gesture_.new_action;
-        event->data.action = handler->gesture_.action;
-        event->data.key_code = handler->gesture_.key_code;
-        event->data.slide_value = handler->gesture_.slide_value;
-        event->data.click_count = handler->gesture_.click_count;
-        event->data.long_press_time = handler->gesture_.long_press_time;
-        async->data = (void*)event;
-        uv_async_init(uv_default_loop(), async,
-                      InputEventHandler::OnGestureEvent);
-        uv_async_send(async);
-      }
+int InputEventHandler::start() {
+  return uv_queue_work(uv_default_loop(), &req,
+                       InputEventHandler::DoStart,
+                       InputEventHandler::AfterStart);
+}
+
+int InputEventHandler::stop() {
+  return uv_cancel((uv_req_t*)&req);
+}
+
+void InputEventHandler::DoStart(uv_work_t* req) {
+  InputEventHandler* handler = (InputEventHandler*)req->data;
+  while (true) {
+    daemon_start_listener(&handler->keyevent_,
+                          &handler->gesture_);
+    // Send InputKeyEvent
+    if (handler->keyevent_.new_action) {
+      uv_async_t* async = new uv_async_t;
+      InputKeyEvent* event = new InputKeyEvent();
+      event->event_handler = handler;
+      event->data.new_action = handler->keyevent_.new_action;
+      event->data.value = handler->keyevent_.value;
+      event->data.action = handler->keyevent_.action;
+      event->data.key_code = handler->keyevent_.key_code;
+      event->data.key_time = handler->keyevent_.key_time;
+      async->data = (void*)event;
+      uv_async_init(uv_default_loop(), async,
+                    InputEventHandler::OnKeyEvent);
+      uv_async_send(async);
+    }
+    // Send InputGestureEvent
+    if (handler->gesture_.new_action) {
+      uv_async_t* async = new uv_async_t;
+      InputGestureEvent* event = new InputGestureEvent();
+      event->event_handler = handler;
+      event->data.new_action = handler->gesture_.new_action;
+      event->data.action = handler->gesture_.action;
+      event->data.key_code = handler->gesture_.key_code;
+      event->data.slide_value = handler->gesture_.slide_value;
+      event->data.click_count = handler->gesture_.click_count;
+      event->data.long_press_time = handler->gesture_.long_press_time;
+      async->data = (void*)event;
+      uv_async_init(uv_default_loop(), async,
+                    InputEventHandler::OnGestureEvent);
+      uv_async_send(async);
     }
   }
-  static void AfterStart(uv_work_t* req, int status) {
-    // TODO
-  }
-  static void OnKeyEvent(uv_async_t* async) {
-    InputKeyEvent* event = (InputKeyEvent*)async->data;
-  }
-  static void OnGestureEvent(uv_async_t* async) {
-    InputGestureEvent* event = (InputGestureEvent*)async->data;
-  }
+}
 
- private:
-  iotjs_input_t* inputwrap;
-  struct keyevent keyevent_;
-  struct gesture gesture_;
-  uv_work_t req;
+void InputEventHandler::AfterStart(uv_work_t* req, int status) {
+  fprintf(stdout, "input event handler stoped\n");
+}
+
+void InputEventHandler::OnKeyEvent(uv_async_t* async) {
+  InputKeyEvent* event = (InputKeyEvent*)async->data;
+  iotjs_input_t* input = event->event_handler->inputwrap;
+  IOTJS_VALIDATED_STRUCT_METHOD(iotjs_input_t, input);
+
+  jerry_value_t jthis = iotjs_jobjectwrap_jobject(&_this->jobjectwrap);
+  jerry_value_t onevent = iotjs_jval_get_property(jthis, "onevent");
+  if (!jerry_value_is_function(onevent)) {
+    fprintf(stderr, "no onevent function is registered\n");
+    return;
+  }
+  uint32_t jargc = 4;
+  jerry_value_t jargv[jargc] = {
+    jerry_create_number((double)event->data.value),
+    jerry_create_number((double)event->data.action),
+    jerry_create_number((double)event->data.key_code),
+    jerry_create_number((double)event->data.key_time),
+  };
+  jerry_call_function(onevent, jerry_create_undefined(), jargv, jargc);
+  for (int i = 0; i < jargc; i++) {
+    jerry_release_value(jargv[i]);
+  }
+  jerry_release_value(onevent);
+}
+
+void InputEventHandler::OnGestureEvent(uv_async_t* async) {
+  // Gesture TODO
 }
 
 iotjs_input_t* iotjs_input_create(const jerry_value_t jinput) {
@@ -160,6 +187,12 @@ iotjs_input_t* iotjs_input_create(const jerry_value_t jinput) {
   _this->initializer = new InputInitializer(inputwrap);
   _this->event_handler = new InputEventHandler(inputwrap);
   return inputwrap;
+}
+
+void iotjs_input_destroy(iotjs_input_t* input) {
+  IOTJS_VALIDATED_STRUCT_DESTRUCTOR(iotjs_input_t, input);
+  iotjs_jobjectwrap_destroy(&_this->jobjectwrap);
+  IOTJS_RELEASE(input);
 }
 
 JS_FUNCTION(Input) {
@@ -177,7 +210,6 @@ JS_FUNCTION(Start) {
   int timeout_select = JS_GET_ARG(0, number);
   int timeout_dbclick = JS_GET_ARG(1, number);
   int timeout_slide = JS_GET_ARG(2, number);
-
   int r = _this->initializer->start(timeout_select,
                                     timeout_dbclick,
                                     timeout_slide);
@@ -190,9 +222,9 @@ JS_FUNCTION(Disconnect) {
 
   if (!_this->initializer->initialized)
     _this->initializer->stop();
-  if (!_this->event_handler->initialized)
+  if (_this->event_handler != NULL)
     _this->event_handler->stop();
-  return jerry_create_number(1);
+  return jerry_create_boolean(true);
 }
 
 void init(jerry_value_t exports) {
