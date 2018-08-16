daded7aa4e1e0a241dc65051eec19468c133b9e6
Yorkie Liu <yorkiefixer@gmail.com>
2018-08-16 21:14:49 +0800

    app, runtime: add activity
    Change-Id: Idf6e5e77ada37fad5ccfcb4b978726f3016ceb1e

--- runtime/app/lightApp.js
+++ runtime/app/lightApp.js
@@ -2,20 +2,16 @@
 
 var inherits = require('util').inherits;
 var EventEmitter = require('events').EventEmitter;
-var property = require('property');
-
+var createActivity = require('./activity').createActivity;
 var MEDIA_SOURCE = '/opt/media/';
 
 function Client(appId, runtime) {
-  var self = this;
   EventEmitter.call(this);
-
+  
+  var self = this;
   this.appHome = '';
   this.runtime = runtime;
   this.appId = appId;
-  // 创建隔离的App
-  var app = new EventEmitter();
-  this.app = app;
 
   this.on('create', this._onCreate.bind(this));
   this.on('pause', this._onPaused.bind(this));
@@ -24,10 +20,9 @@ function Client(appId, runtime) {
   this.on('voice_command', this._onVoiceCommand.bind(this));
   this.on('key_event', this._onKeyEvent.bind(this));
 
-  var adapter = new runtime.adapter(runtime.service);
-  this.adapter = adapter;
-
+  var adapter = this.adapter = new runtime.adapter(runtime.service);
   this.ttsCallback = {};
+
   adapter.listenTtsdEvent((name, args) => {
     // ttsd的event事件
     if (name === 'ttsdevent') {
@@ -61,168 +56,42 @@ function Client(appId, runtime) {
     console.log('mediad listen error', err);
   });
 
-  //------------------------ 给App注入服务 -------------------------------
-  app.getAppId = function () {
-    return appId;
-  };
-  app.exit = function () {
-    return self.runtime.exitAppById(appId);
-  };
-  app.destroyAll = function () {
-    return self.runtime.destroyAll();
-  };
-  app.setPickup = function (isPickup) {
-    return self.runtime.setPickup(isPickup === true ? true : false);
-  };
-  app.mockNLPResponse = function (nlp, action) {
-    self._onVoiceCommand(nlp, action);
-  };
-  // tts module
-  app.tts = {
-    speak: function (text, cb) {
-      self.adapter.ttsMethod('speak', [appId, text])
-        .then((args) => {
-          // 返回的参数是一个数组，按顺序
-          logger.log('tts register', args[0]);
-          self.ttsCallback['ttscb:' + args[0]] = cb.bind(app);
-        })
-        .catch((err) => {
-          logger.error(err);
-        });
-    },
-    stop: function (cb) {
-      self.adapter.ttsMethod('stop', [appId])
-        .then((args) => {
-          cb.call(app, null);
-        })
-        .catch((err) => {
-          cb.call(app, err);
-        });
-    }
-  };
-  // media模块
-  app.media = {
-    start: function (url, cb) {
-      self.adapter.multiMediaMethod('start', [appId, url])
-        .then((args) => {
-          logger.log('media register', args);
-          self.multiMediaCallback['mediacb:' + args[0]] = cb.bind(app);
-        })
-        .catch((err) => {
-          logger.error(err);
-        });
-    },
-    pause: function (cb) {
-      self.adapter.multiMediaMethod('pause', [appId])
-        .then((args) => {
-          cb.call(app, null);
-        })
-        .catch((err) => {
-          cb.call(app, null);
-        });
-    },
-    resume: function (cb) {
-      self.adapter.multiMediaMethod('resume', [appId])
-        .then((args) => {
-          cb.call(app, null);
-        })
-        .catch((err) => {
-          cb.call(app, null);
-        });
-    },
-    stop: function (cb) {
-      self.adapter.multiMediaMethod('stop', [appId])
-        .then((args) => {
-          cb.call(app, null);
-        })
-        .catch((err) => {
-          cb.call(app, null);
-        });
-    }
-  };
-  // light module
-  app.light = {
-    play: function (name, args) {
-      return self.adapter.lightMethod('play', [appId, name, JSON.stringify(args || [])]);
-    },
-    stop: function () {
-      return self.adapter.lightMethod('stop', [appId]);
-    }
-  };
-  app.playSound = function (name) {
-    var len = name.length;
-    var absPath;
-    // etc.. system://path/to/sound.ogg
-    if (len > 9 && name.substr(0, 9) === 'system://') {
-      absPath = MEDIA_SOURCE + name.substr(9);
-    // etc.. self://path/to/sound.ogg
-    } else if (len > 7 && name.substr(0, 7) === 'self://') {
-      absPath = self.appHome + '/' + name.substr(7);
-    // etc.. path/to/sound.ogg
-    } else {
-      absPath = self.appHome + '/' + name;
-    }
-    return self.adapter.lightMethod('appSound', [appId, absPath]);
-  };
-  app.localStorage = {
-    getItem: function (key) {
-      return property.get(key);
-    },
-    setItem: function (key, value) {
-      return property.set(key, value);
-    }
-  };
+  // 创建隔离的App
+  this.app = createActivity(appId, this);
 }
 inherits(Client, EventEmitter);
 
-/**
- * @method _onCreate
- */
 Client.prototype._onCreate = function () {
   this.state = 'created';
   this.app.emit('created');
-}
-/**
- * @method _onPaused
- */
+};
+
 Client.prototype._onPaused = function () {
   this.state = 'paused';
   this.app.emit('paused');
-}
-/**
- * @method _onResumed
- */
+};
+
 Client.prototype._onResumed = function () {
   this.state = 'resumed';
   this.app.emit('resumed');
-}
+};
 
-/**
- * @method _onDestroyed
- */
 Client.prototype._onDestroyed = function () {
   this.state = 'destroyed';
   this.app.emit('destroyed');
-}
-/**
- * @method _onVoiceCommand
- */
+};
+
 Client.prototype._onVoiceCommand = function (nlp, action) {
   this.state = 'voice_command';
   this.app.emit('onrequest', nlp, action);
-}
-/**
- * @method _onKeyEvent
- */
+};
+
 Client.prototype._onKeyEvent = function () {
   this.app.emit('keyEvent');
-}
-/**
- * 通知App manager该应用应该被销毁了
- */
+};
+
 Client.prototype.exit = function () {
   this.runtime.exitAppById(this.appId);
-}
-
+};
 
 module.exports = Client;
