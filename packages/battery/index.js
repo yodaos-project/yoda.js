55074cfbc2f983d8e2d916bb66b9eec1cf9e931e
Wu Chengzhong <chengzhong.wu@rokid.com>
2018-08-20 19:43:17 +0800

    fix: ffi usage in namespace battery
    Change-Id: I921ceb8849691887e79996c3bce8127f68ad1751

--- packages/battery/index.js
+++ packages/battery/index.js
@@ -1,20 +1,14 @@
-'use strict';
+'use strict'
 
 /**
  * @namespace battery
  * @description provide the battery state management.
  */
 
-var ffi = require('ffi');
-var battery = ffi.Library('libpower_ctrl.so', {
-  /**
-   * @memberof battery
-   * @function get
-   * @param {BatteryState} powerState a pre-allocated PowerState memory block
-   * @returns {number} status code, 0 if success, non-zero code otherwise
-   */
-  'get': [ 'int', [ 'pointer' ] ]
-});
+var ffi = require('ffi')
+var lib = ffi.Library('libpower_ctrl.so', {
+  'power_get_property': [ 'int', [ 'pointer' ] ]
+})
 
 var fields = [
   [ 'status', 'int' ],
@@ -34,31 +28,38 @@ var fields = [
   [ 'time_to_full', 'int' ],
   [ 'charge_full', 'int' ],
   [ 'energy_avg', 'int' ]
-];
-
-/**
- * @memberof battery
- * @constructor
- * @param {Object} [fields] initialized with given fields, leaves unspecified fields uninitialized
- * @param {number} [fields.status]
- * @param {number} [fields.online]
- * @param {number} [fields.vbus_type]
- * @param {number} [fields.charge_current]
- * @param {number} [fields.temp]
- * @param {number} [fields.capacity]
- * @param {number} [fields.health]
- * @param {number} [fields.battery_present]
- * @param {number} [fields.voltagenow]
- * @param {number} [fields.voltagemax]
- * @param {number} [fields.voltagemin]
- * @param {number} [fields.currentnow]
- * @param {number} [fields.time_to_empty_now]
- * @param {number} [fields.time_to_empty_avg]
- * @param {number} [fields.time_to_full]
- * @param {number} [fields.charge_full]
- * @param {number} [fields.energy_avg]
- */
+]
 var BatteryState = ffi.Struct(fields)
-batter.BatteryState = BatteryState;
-module.exports = battery;
 
+module.exports = {
+  /**
+   * @memberof battery
+   * @function get
+   * @param {BatteryState} powerState a pre-allocated PowerState memory block
+   * @returns {Number} status code, 0 if success, non-zero code otherwise
+   */
+  get: lib.power_get_property,
+  /**
+   * @memberof battery
+   * @constructor
+   * @param {Object} [fields] initialized with given fields, leaves unspecified fields uninitialized
+   * @param {Number} [fields.status]
+   * @param {Number} [fields.online]
+   * @param {Number} [fields.vbus_type]
+   * @param {Number} [fields.charge_current]
+   * @param {Number} [fields.temp]
+   * @param {Number} [fields.capacity]
+   * @param {Number} [fields.health]
+   * @param {Number} [fields.battery_present]
+   * @param {Number} [fields.voltagenow]
+   * @param {Number} [fields.voltagemax]
+   * @param {Number} [fields.voltagemin]
+   * @param {Number} [fields.currentnow]
+   * @param {Number} [fields.time_to_empty_now]
+   * @param {Number} [fields.time_to_empty_avg]
+   * @param {Number} [fields.time_to_full]
+   * @param {Number} [fields.charge_full]
+   * @param {Number} [fields.energy_avg]
+   */
+  BatteryState: BatteryState
+}
