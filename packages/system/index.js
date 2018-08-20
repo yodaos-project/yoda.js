58adb27687510917d82b41ee8cb086e57a1450df
Yorkie Liu <yorkiefixer@gmail.com>
2018-08-20 16:22:58 +0800

    api change: update system api, redesign it
    Change-Id: I34a3c1639ec18f959644baa6dfb33bfe957aa306

--- packages/system/index.js
+++ packages/system/index.js
@@ -7,34 +7,31 @@
 var native = require('./system.node');
 
 /**
+ * @var {String} OTA_IMAGE_URI - The uri of your ota image, "/data/upgrade/upgrade.img".
+ * @memberof system
+ */
+exports.OTA_IMAGE_URI = '/data/upgrade/upgrade.img';
+
+/**
  * Reboot the system.
  * @memberof system
  * @function reboot
+ * @returns {Boolean}
  */
 exports.reboot = native.reboot;
 
 /**
+ * Verify the OTA image, including hash(md5) check, section check and header check.
  * @memberof system
- * @class ImageUtils
+ * @function verifyOtaImage
+ * @returns {Boolean}
  */
-var ImageUtils = {
-  /**
-   * @memberof system.ImageUtils
-   * @function verify
-   */
-  verify: native.verifyImage,
-  /**
-   * @memberof system.ImageUtils
-   * @function prepare
-   */
-  prepare: native.prepareImage,
-};
+exports.verifyOtaImage = native.verifyOtaImage;
 
 /**
+ * Prepare the OTA image. It should be called before start upgrading.
  * @memberof system
- * get image
+ * @function prepareOtaImage
+ * @returns {Boolean}
  */
-exports.getImage = function() {
-  return ImageUtils;
-};
-
+exports.prepareOtaImage = native.prepareOtaImage;
