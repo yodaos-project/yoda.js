c7a2f8e69a3a109d7fe9c14ca5471b95bc47d121
Yorkie Liu <yorkiefixer@gmail.com>
2018-08-21 19:20:23 +0800

    format the tests
    Change-Id: I16fef4a522a9870cdc74314687360ce649c57d38

--- tests/packages/test-light.js
+++ tests/packages/test-light.js
@@ -1,27 +1,27 @@
-'use strict';
+'use strict'
 
-var test = require('tape');
-var light = require('light');
+var test = require('tape')
+var light = require('light')
 
-test('light get profile', function(t) {
-  var profile = light.getProfile();
-  t.equal(typeof profile.leds, 'number');
-  t.equal(typeof profile.format, 'number');
-  t.equal(typeof profile.maximumFps, 'number');
-  t.equal(typeof profile.micAngle, 'number');
-  t.end();
-});
+test('light get profile', function (t) {
+  var profile = light.getProfile()
+  t.equal(typeof profile.leds, 'number')
+  t.equal(typeof profile.format, 'number')
+  t.equal(typeof profile.maximumFps, 'number')
+  t.equal(typeof profile.micAngle, 'number')
+  t.end()
+})
 
-test('light simple render', function(t) {
-  var profile = light.getProfile();
-  light.enable();
+test('light simple render', function (t) {
+  var profile = light.getProfile()
+  light.enable()
 
-  var buf = new Buffer(profile.leds * profile.format);
-  buf.fill(233);
-  light.write(buf);
+  var buf = Buffer.from(profile.leds * profile.format)
+  buf.fill(233)
+  light.write(buf)
   setTimeout(() => {
-    buf.fill(0);
-    light.write(buf);
-    t.end();
-  }, 1500);
-});
+    buf.fill(0)
+    light.write(buf)
+    t.end()
+  }, 1500)
+})
--- tests/packages/test-logger.js
+++ tests/packages/test-logger.js
@@ -1,20 +1,20 @@
-'use strict';
+'use strict'
 
-process.env.LOG_PORT = 8081;
+process.env.LOG_PORT = 8081
 
-var test = require('tape');
-var net = require('net');
-var logger = require('logger')('log');
+var test = require('tape')
+var net = require('net')
+var logger = require('logger')('log')
 
-test('simple ', function(t) {
-  t.plan(1);
-  var socket = net.connect(process.env.LOG_PORT);
-  var text = 'foobar';
+test('simple ', function (t) {
+  t.plan(1)
+  var socket = net.connect(process.env.LOG_PORT)
+  var text = 'foobar'
   socket.on('data', (buf) => {
-    socket.end();
-    t.assert(buf + '', text);
-    logger.closeServer();
-    t.end();
-  });
-  logger.log(text);
-});
+    socket.end()
+    t.assert(buf + '', text)
+    logger.closeServer()
+    t.end()
+  })
+  logger.log(text)
+})
