7ca048436771284795d5298c1b1b0a0013664ea1
Yorkie Liu <yorkiefixer@gmail.com>
2018-08-21 18:56:01 +0800

    bluetooth: implement broadcast and close intents
    Change-Id: I21bbaf0ee3469634dad956ca8499de4e480bea1b

---
+++ apps/bluetooth/app.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var bluetooth = require('bluetooth');
+var property = require('property');
+
+module.exports = function(activity) {
+
+  var agent = null;
+  var player = null;
+  var name = 'Rokid-Me-072';
+  var STRING_BROADCAST = '蓝牙已打开，请使用手机搜索设备';
+  var STRING_CLOSED = '蓝牙已关闭';
+
+  function broadcast() {
+    speakAndExit(STRING_BROADCAST + name);
+    if (agent === null) {
+      agent = bluetooth.getBluetooth(name);
+    }
+    if (player === null) {
+      player = agent.createPlayer();
+      player.on('open', () => {
+        // auto play when open
+        player.play();
+      });
+    }
+  }
+
+  function disconnect() {
+    speakAndExit(STRING_CLOSED);
+    if (agent) {
+      // TODO
+      agent = null;
+      player = null;
+    }
+  }
+
+  function speakAndExit(text) {
+    return activity.tts.speak(text, () => {
+      activity.exit();
+    });
+  }
+
+  activity.on('onrequest', function(nlp, action) {
+    switch (nlp.intent) {
+    case 'bluetooth_broadcast':
+      broadcast();
+      break;
+    case 'bluetooth_disconnect':
+      disconnect();
+      break;
+    default:
+      activity.exit();
+      break;
+    }
+  });
+};
