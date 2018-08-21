0e42aede53465030a8c1269e9f0c0d4677c8f2cb
Wu Chengzhong <chengzhong.wu@rokid.com>
2018-08-21 17:32:27 +0800

    system: PrepareOta params and returns
    Change-Id: I255ab4e2b7aacb77df053102177edd6794ffb8af

--- packages/system/src/SystemNative.cc
+++ packages/system/src/SystemNative.cc
@@ -1,7 +1,6 @@
 #include "SystemNative.h"
 #include <recovery/recovery.h>
 
-#define UPGRADE_OTA_PATH "/data/upgrade/upgrade.img"
 
 JS_FUNCTION(Reboot) {
   system("reboot");
@@ -12,22 +11,41 @@ JS_FUNCTION(VerifyOtaImage) {
   return jerry_create_boolean(true);
 }
 
-JS_FUNCTION(PrepareOtaImage) {
+JS_FUNCTION(PrepareOta) {
+  iotjs_string_t iotjs_path = JS_GET_ARG(0, string);
+  const char* path = iotjs_string_data(&iotjs_path);
+  iotjs_string_destroy(&iotjs_path);
+
+  const char *mode, *state;
+  if (strlen(path) == 0) {
+    /**
+     * clear recovery state
+     */
+    mode = "initmode";
+    state = "";
+  } else {
+    mode = BOOTMODE_RECOVERY;
+    state = BOOTSTATE_READY
+  }
+
+  int status;
   struct boot_cmd cmd;
   memset(&cmd, 0, sizeof(cmd));
-  get_recovery_cmd_status(&cmd);
-  strncpy(cmd.boot_mode, BOOTMODE_RECOVERY, strlen(BOOTMODE_RECOVERY));
-  strncpy(cmd.recovery_path, UPGRADE_OTA_PATH, strlen(UPGRADE_OTA_PATH));
-  strncpy(cmd.recovery_state, BOOTSTATE_READY, strlen(BOOTSTATE_READY));
-  set_recovery_cmd_status(&cmd);
-  return jerry_create_boolean(true);
+  status = get_recovery_cmd_status(&cmd);
+  if (status != 0) {
+    return jerry_create_number(status);
+  }
+  strncpy(cmd.boot_mode, mode, strlen(mode));
+  strncpy(cmd.recovery_path, path, strlen(path));
+  strncpy(cmd.recovery_state, state, strlen(state));
+  status = set_recovery_cmd_status(&cmd);
+  return jerry_create_number(status);
 }
 
 void init(jerry_value_t exports) {
   iotjs_jval_set_method(exports, "verifyOtaImage", VerifyOtaImage);
-  iotjs_jval_set_method(exports, "prepareOtaImage", PrepareOtaImage);
+  iotjs_jval_set_method(exports, "prepareOta", PrepareOta);
   iotjs_jval_set_method(exports, "reboot", Reboot);
 }
 
 NODE_MODULE(system, init)
-
