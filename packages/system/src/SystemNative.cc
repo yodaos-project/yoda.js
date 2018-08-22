ed49456ffc1a6b77b859a8fd170dcb4bbc913571
Wu Chengzhong <chengzhong.wu@rokid.com>
2018-08-22 13:30:01 +0800

    feat: add system.diskUsage
    Change-Id: I5622691e40d866681ad81f86f7418a2c680c78ea

--- packages/system/src/SystemNative.cc
+++ packages/system/src/SystemNative.cc
@@ -1,10 +1,11 @@
 #include "SystemNative.h"
+#include <sys/statvfs.h>
 #include <recovery/recovery.h>
 
 
 JS_FUNCTION(Reboot) {
-  system("reboot");
-  return jerry_create_boolean(true);
+  int ret = system("reboot");
+  return jerry_create_number(ret);
 }
 
 JS_FUNCTION(VerifyOtaImage) {
@@ -14,7 +15,6 @@ JS_FUNCTION(VerifyOtaImage) {
 JS_FUNCTION(PrepareOta) {
   iotjs_string_t iotjs_path = JS_GET_ARG(0, string);
   const char* path = iotjs_string_data(&iotjs_path);
-  iotjs_string_destroy(&iotjs_path);
 
   const char *mode, *state;
   if (strlen(path) == 0) {
@@ -25,7 +25,7 @@ JS_FUNCTION(PrepareOta) {
     state = "";
   } else {
     mode = BOOTMODE_RECOVERY;
-    state = BOOTSTATE_READY
+    state = BOOTSTATE_READY;
   }
 
   int status;
@@ -33,19 +33,42 @@ JS_FUNCTION(PrepareOta) {
   memset(&cmd, 0, sizeof(cmd));
   status = get_recovery_cmd_status(&cmd);
   if (status != 0) {
+    iotjs_string_destroy(&iotjs_path);
     return jerry_create_number(status);
   }
   strncpy(cmd.boot_mode, mode, strlen(mode));
   strncpy(cmd.recovery_path, path, strlen(path));
   strncpy(cmd.recovery_state, state, strlen(state));
   status = set_recovery_cmd_status(&cmd);
+
+  iotjs_string_destroy(&iotjs_path);
   return jerry_create_number(status);
+}
+
+JS_FUNCTION(DiskUsage) {
+  iotjs_string_t iotjs_path = JS_GET_ARG(0, string);
+  const char* path = iotjs_string_data(&iotjs_path);
+
+  struct statvfs info = {};
+  int ret = statvfs(path, &info);
+  iotjs_string_destroy(&iotjs_path);
+  if (ret) {
+    int errnum = errno;
+    return jerry_create_error(JERRY_ERROR_COMMON, (jerry_char_t*)strerror(errnum));
+  }
+
+  jerry_value_t res = jerry_create_object();
+  iotjs_jval_set_property_number(res, "available", info.f_bavail * info.f_frsize);
+  iotjs_jval_set_property_number(res, "free", info.f_bfree * info.f_frsize);
+  iotjs_jval_set_property_number(res, "total", info.f_blocks * info.f_frsize);
+  return res;
 }
 
 void init(jerry_value_t exports) {
   iotjs_jval_set_method(exports, "verifyOtaImage", VerifyOtaImage);
   iotjs_jval_set_method(exports, "prepareOta", PrepareOta);
   iotjs_jval_set_method(exports, "reboot", Reboot);
+  iotjs_jval_set_method(exports, "diskUsage", DiskUsage);
 }
 
 NODE_MODULE(system, init)
