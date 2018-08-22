ed81292e33ce18acdb0802794b7ff45cd7f2adb0
Yorkie Liu <yorkiefixer@gmail.com>
2018-08-22 16:32:06 +0800

    use standardjs for resources
    Change-Id: I4aea433f324c5cffcacfbd91b3d3075a6684f8b0

--- res/light/loading.js
+++ res/light/loading.js
@@ -1,27 +1,27 @@
 'use strict'
 
-module.exports = function loading(light, data, callback) {
-  var pos = 0;
+module.exports = function loading (light, data, callback) {
+  var pos = 0
   if (data.degree) {
-    pos = Math.floor((data.degree / 360) * light.ledsConfig.leds);
+    pos = Math.floor((data.degree / 360) * light.ledsConfig.leds)
   }
   var render = function () {
-    light.fill(30, 30, 150);
-    pos = pos === 11 ? 0 : pos + 1;
-    light.pixel(pos, 255, 255, 255);
-    light.render();
+    light.fill(30, 30, 150)
+    pos = pos === 11 ? 0 : pos + 1
+    light.pixel(pos, 255, 255, 255)
+    light.render()
     light.requestAnimationFrame(() => {
-      render();
-    }, 60);
-  };
-  render();
+      render()
+    }, 60)
+  }
+  render()
   light.requestAnimationFrame(() => {
-    light.stop();
-  }, 6000);
+    light.stop()
+  }, 6000)
 
   return {
     stop: function () {
-      light.stop();
+      light.stop()
     }
-  };
-};
+  }
+}
