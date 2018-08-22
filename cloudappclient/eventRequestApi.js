af05b555cbab00c5493540d32c535e075a787974
Yorkie Liu <yorkiefixer@gmail.com>
2018-08-22 15:58:12 +0800

    use standardjs for cloudappclient source
    Change-Id: I05c85d0ffb10f081b1a749592f2487af3ad958ac

--- cloudappclient/eventRequestApi.js
+++ cloudappclient/eventRequestApi.js
@@ -1,24 +1,24 @@
-'use strict';
+'use strict'
 
-var https = require('https');
-var crypto = require('crypto');
-var qs = require('querystring');
-var logger = require('logger')('eventReq');
+var https = require('https')
+var crypto = require('crypto')
+var qs = require('querystring')
+var logger = require('logger')('eventReq')
 
-var CONFIG = null;
-var DEFAULT_HOST = 'apigwrest.open.rokid.com';
-var DEFAULT_URI = '/v1/skill/dispatch/sendEvent';
+var CONFIG = null
+var DEFAULT_HOST = 'apigwrest.open.rokid.com'
+var DEFAULT_URI = '/v1/skill/dispatch/sendEvent'
 
-function gensigh(data) {
+function gensigh (data) {
   return crypto.createHash('md5')
     .update(qs.stringify(data))
     .digest('hex')
-    .toUpperCase();
+    .toUpperCase()
 }
 
-function getAuth() {
+function getAuth () {
   if (CONFIG === null) {
-    return '';
+    return ''
   }
   var data = {
     key: CONFIG.key,
@@ -28,7 +28,7 @@ function getAuth() {
     version: '1',
     time: Math.floor(Date.now() / 1000),
     secret: CONFIG.secret
-  };
+  }
   return [
     `version=${data.version}`,
     `time=${data.time}`,
@@ -36,23 +36,23 @@ function getAuth() {
     `key=${data.key}`,
     `device_type_id=${data.device_type_id}`,
     `device_id=${data.device_id}`,
-    `service=${data.service}`,
-  ].join(';');
+    `service=${data.service}`
+  ].join(';')
 }
 
 function setConfig (config) {
-  CONFIG = config;
+  CONFIG = config
 };
 
-function request(event, appId, options, onaction) {
+function request (event, appId, options, onaction) {
   var data = {
     event: event,
     appId: appId,
     extra: JSON.stringify(options)
-  };
-  logger.log('event:', data);
+  }
+  logger.log('event:', data)
 
-  data = JSON.stringify(data);
+  data = JSON.stringify(data)
   var req = https.request({
     method: 'POST',
     host: DEFAULT_HOST,
@@ -63,26 +63,26 @@ function request(event, appId, options, onaction) {
       'Content-Length': data.length
     }
   }, (res) => {
-    var list = [];
-    res.on('data', (chunk) => list.push(chunk));
+    var list = []
+    res.on('data', (chunk) => list.push(chunk))
     res.on('end', () => {
-      var msg = Buffer.concat(list).toString();
+      var msg = Buffer.concat(list).toString()
       if (res.statusCode !== 200) {
-        logger.error(`Error: failed upload ${event} ${data} with ${msg}`);
+        logger.error(`Error: failed upload ${event} ${data} with ${msg}`)
       } else {
-        msg = JSON.parse(msg);
+        msg = JSON.parse(msg)
         // logger.log(`got ${event} successfully response`, msg);
         if (typeof onaction === 'function') {
-          onaction(msg.response);
+          onaction(msg.response)
         }
       }
-    });
-  });
+    })
+  })
   req.on('error', (err) => {
-    logger.error(err && err.stack);
-  });
-  req.write(data);
-  req.end();
+    logger.error(err && err.stack)
+  })
+  req.write(data)
+  req.end()
 };
 
 function ttsEvent (name, appId, itemId, cb) {
@@ -90,16 +90,16 @@ function ttsEvent (name, appId, itemId, cb) {
     voice: {
       itemId: itemId
     }
-  }, cb);
+  }, cb)
 }
 
 function mediaEvent (name, appId, extra, cb) {
   request(name, appId, {
     media: extra
-  }, cb);
+  }, cb)
 }
 
-exports.request = request;
-exports.ttsEvent = ttsEvent;
-exports.mediaEvent = mediaEvent;
-exports.setConfig = setConfig;
+exports.request = request
+exports.ttsEvent = ttsEvent
+exports.mediaEvent = mediaEvent
+exports.setConfig = setConfig
