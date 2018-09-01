638344127e5435c54210a1b8275a104939344c14
Yorkie Liu <yorkiefixer@gmail.com>
2018-09-01 18:17:40 +0800

    @yoda/bluetooth: add the new module bluetooth
    Change-Id: Id91bdd90bbdba5ee7b6be619c9bb1fe9e5875492

---
+++ packages/@yoda/bluetooth/index.js
@@ -0,0 +1,42 @@
+'use strict'
+
+/**
+ * @module @yoda/bluetooth
+ * @description The YodaOS includes support for the Bluetooth network
+ * stack, which allows a device to wirelessly exchange data with other
+ * Bluetooth devices. Using the Bluetooth APIs, your application can
+ * perform the followings:
+ *
+ * - Control Bluetooth playback.
+ * - Transfer data to and from other devices.
+ */
+
+var messageStreamInstance
+var playerInstance
+
+module.exports = {
+  /**
+   * get the `BluetoothMessageStream` instance for messaging.
+   * @returns {module:@yoda/bluetooth.BluetoothMessageStream}
+   */
+  getMessageStream: function () {
+    if (!messageStreamInstance) {
+      var BluetoothMessageStream =
+        require('./lib/stream').BluetoothMessageStream
+      messageStreamInstance = new BluetoothMessageStream()
+    }
+    return messageStreamInstance
+  },
+  /**
+   * get the `BluetoothPlayer` instance for multimedia.
+   * @returns {module:@yoda/bluetooth.BluetoothPlayer}
+   */
+  getPlayer: function () {
+    if (!playerInstance) {
+      var BluetoothPlayer =
+        require('./lib/player').BluetoothPlayer
+      playerInstance = new BluetoothPlayer()
+    }
+    return playerInstance
+  }
+}
---
+++ packages/@yoda/bluetooth/lib/player.js
@@ -0,0 +1,110 @@
+'use strict'
+
+var inherits = require('util').inherits
+var EventEmitter = require('events').EventEmitter
+var helper = require('./helper')
+var a2dpsinkPath = 'ipc:///var/run/bluetooth/a2dpsink_event'
+
+/**
+ * Use `bluetooth.getPlayer()` instead of this constructor.
+ * @class
+ * @augments EventEmitter
+ * @memberof module:@yoda/bluetooth
+ */
+function BluetoothPlayer () {
+  EventEmitter.call(this)
+  this._cmdSocket = helper.getCmdSocket()
+  this._eventSocket = helper.getSocket(a2dpsinkPath)
+  this._eventSocket.on('message', (buffer) => {
+    try {
+      /**
+       * When play state updates.
+       * @event module:@yoda/bluetooth.BluetoothPlayer#stateupdate
+       * @type {Object}
+       * @property {String} a2dpstate - the a2dp state
+       * @property {String} connect_state - if the connect
+       * @property {String} connect_name - the connected device name
+       * @property {String} play_state - the state of playing on the peer device
+       */
+      var msg = JSON.parse(buffer + '')
+      this.emit('stateupdate', msg)
+    } catch (err) {
+      /**
+       * When something is wrong.
+       * @event module:@yoda/bluetooth.BluetoothPlayer#error
+       * @type {Error}
+       */
+      this.emit('error', err)
+    }
+  })
+}
+inherits(BluetoothPlayer, EventEmitter)
+
+/**
+ * @private
+ */
+BluetoothPlayer.prototype._send = function (cmdstr) {
+  return this._cmdSocket.send(JSON.stringify({
+    proto: 'A2DPSINK',
+    command: cmdstr
+  }))
+}
+
+/**
+ * Start the bluetooth player.
+ * @returns {Null}
+ */
+BluetoothPlayer.prototype.start = function start () {
+  return this._send('ON')
+}
+
+/**
+ * End the bluetooth player.
+ * @returns {Null}
+ */
+BluetoothPlayer.prototype.end = function () {
+  this._eventSocket.removeAllListeners()
+  return this._send('OFF')
+}
+
+/**
+ * Play the music.
+ * @returns {Null}
+ */
+BluetoothPlayer.prototype.play = function () {
+  return this._send('PLAY')
+}
+
+/**
+ * Stop the music.
+ * @returns {Null}
+ */
+BluetoothPlayer.prototype.stop = function () {
+  return this._send('STOP')
+}
+
+/**
+ * Pause the music.
+ * @returns {Null}
+ */
+BluetoothPlayer.prototype.pause = function () {
+  return this._send('PAUSE')
+}
+
+/**
+ * Play next music.
+ * @returns {Null}
+ */
+BluetoothPlayer.prototype.next = function () {
+  return this._send('NEXT')
+}
+
+/**
+ * Play previous music.
+ * @returns {Null}
+ */
+BluetoothPlayer.prototype.prev = function () {
+  return this._send('PREV')
+}
+
+exports.BluetoothPlayer = BluetoothPlayer
