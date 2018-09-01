4491540c9757888eadfc7aefbddf9a808eca89de
Yorkie Liu <yorkiefixer@gmail.com>
2018-09-01 11:32:38 +0800

    vui: start until turen rpc connected
    Change-Id: Ife4ef5eeb1ca385d4be6b2d8aff49b4b3a22178c

--- runtime/services/vuid/index.js
+++ runtime/services/vuid/index.js
@@ -1,94 +1,74 @@
 'use strict'
 
-var Turen = require('turen')
 var cloudApi = require('../../lib/cloudapi')
 var property = require('@yoda/property')
+var TurenSpeech = require('turen').TurenSpeech
 var AudioManager = require('@yoda/audio').AudioManager
 var AppRuntime = require('../../lib/app-runtime')
+var CloudGW = require('@yoda/cloudgw')
 var logger = require('logger')('main')
 
-var CloudGW = require('@yoda/cloudgw')
-
-// ------------------------------------------------------
-
-var runtime = new AppRuntime(['/opt/apps'])
-
-runtime.volume = AudioManager
-runtime.cloudApi = cloudApi
-
-AudioManager.setMute(false)
-AudioManager.setVolume(60)
-
-var speech = new Turen.TurenSpeech()
 var speechT = require('@yoda/speech')
+var speechV = new TurenSpeech()
+speechV.start()
+speechV.on('ready', entry)
 
-// 监听代理事件。代理执行turen API
-runtime.on('setStack', function (stack) {
-  speech.setStack(stack)
-  logger.log('setStack ', stack)
-})
-runtime.on('setPickup', function (isPickup) {
-  speech.setPickup(isPickup)
-  logger.log('setPickup ', isPickup)
-})
+;(function init () {
+  // if DEBUG, we put raw events
+  if (process.env.DISPLAY_RAW_EVENT) {
+    speechV.on('raw event', function (event) {
+      logger.log(event)
+    })
+  }
+  AudioManager.setMute(false)
+  AudioManager.setVolume(60)
+})()
 
-speech.on('voice coming', function (event) {
-  logger.log('voice coming')
-  runtime.onEvent('voice coming', {})
-})
-speech.on('voice local awake', function (event) {
-  logger.log('voice local awake')
-  runtime.onEvent('voice local awake', event)
-})
-speech.on('asr pending', function (asr) {
-  logger.log('asr pending', asr)
-  runtime.onEvent('asr pending', asr)
-})
-speech.on('asr end', function (asr, event) {
-  logger.log('asr end', asr)
-  runtime.onEvent('asr end', {
-    asr: asr
+function entry () {
+  console.log('ready')
+  var runtime = new AppRuntime(['/opt/apps'])
+  runtime.cloudApi = cloudApi
+  runtime.volume = AudioManager
+
+  runtime.on('setStack', function onSetStack (stack) {
+    logger.log('setStack ', stack)
+    speechV.setStack(stack)
   })
-})
-
-// if DEBUG, we put raw events
-if (process.env.DISPLAY_RAW_EVENT) {
-  speech.on('raw event', function (event) {
-    logger.log(event)
+  runtime.on('setPickup', function onSetPickup (isPickup) {
+    logger.log('setPickup ', isPickup)
+    speechV.setPickup(isPickup)
   })
-}
+  speechV.on('voice coming', function onVoiceComing (event) {
+    logger.log('voice coming')
+    runtime.onEvent('voice coming', {})
+  })
+  speechV.on('voice local awake', function onVoiceAwake (event) {
+    logger.log('voice local awake')
+    runtime.onEvent('voice local awake', event)
+  })
+  speechV.on('asr pending', function onAsrPending (asr) {
+    logger.log(`asr pending ${asr}`)
+    runtime.onEvent('asr pending', asr)
+  })
+  speechV.on('asr end', function onAsrComplete (asr, event) {
+    logger.log(`asr end ${asr}`)
+    runtime.onEvent('asr end', {
+      asr: asr
+    })
+  })
+  speechV.on('nlp', function (response, event) {
+    logger.log('nlp', response)
+    runtime.onEvent('nlp', response)
+  })
 
-// 监听turen NLP事件
-speech.on('nlp', function (response, event) {
-  logger.log('nlp', response)
-  runtime.onEvent('nlp', response)
-})
+  runtime.on('reconnected', function () {
+    logger.log('yoda reconnected')
 
-speech.start()
-
-runtime.on('reconnected', function () {
-  logger.log('yoda reconnected')
-  // 登录、绑定、注册mqtt
-  cloudApi.connect().then((mqttAgent) => {
-    // 系统配置文件
-    var config = mqttAgent.config
-    var options = {
-      host: config.host,
-      port: config.port,
-      key: config.key,
-      secret: config.secret,
-      deviceTypeId: config.deviceTypeId,
-      deviceId: config.deviceId
-    }
-    speech.start(options)
-    speechT.start(options)
-    require('@yoda/ota/network').cloudgw = new CloudGW(options)
-
-    // Implementation interface
-    runtime.onGetPropAll = function () {
-      var masterId = property.get('persist.system.user.userId')
-      return {
-        masterId: masterId,
+    // login -> bind -> mqtt
+    cloudApi.connect().then((mqttAgent) => {
+      // load the system configuration
+      var config = mqttAgent.config
+      var options = {
         host: config.host,
         port: config.port,
         key: config.key,
@@ -96,62 +76,73 @@ runtime.on('reconnected', function () {
         deviceTypeId: config.deviceTypeId,
         deviceId: config.deviceId
       }
-    }
+      speechV.start(options)
+      speechT.start(options)
+      require('@yoda/ota/network').cloudgw = new CloudGW(options)
 
-    runtime.onReLogin()
-
-    mqttAgent.on('asr', function (asr) {
-      speechT.getNlpResult(asr, function (err, nlp, action) {
-        if (err) {
-          console.error(`occurrs some error in speechT`)
-        } else {
-          runtime.onVoiceCommand(asr, nlp, action)
+      // implementation interface
+      runtime.onGetPropAll = function () {
+        var masterId = property.get('persist.system.user.userId')
+        return {
+          masterId: masterId,
+          host: config.host,
+          port: config.port,
+          key: config.key,
+          secret: config.secret,
+          deviceTypeId: config.deviceTypeId,
+          deviceId: config.deviceId
         }
-      })
-    })
-    mqttAgent.on('cloud_forward', function (data) {
-      runtime.onCloudForward(data)
-    })
-    mqttAgent.on('get_volume', function (data) {
-      var res = {
-        type: 'Volume',
-        event: 'ON_VOLUME_CHANGE',
-        template: JSON.stringify({
-          mediaCurrent: '' + AudioManager.getVolume(AudioManager.STREAM_AUDIO),
-          mediaTotal: '100',
-          alarmCurrent: '' + AudioManager.getVolume(AudioManager.STREAM_ALARM),
-          alarmTotal: '100'
-        }),
-        appid: ''
       }
-      logger.log('response topic get_volume ->', res)
-      mqttAgent.sendToApp('event', JSON.stringify(res))
-    })
-    mqttAgent.on('set_volume', function (data) {
-      var msg = JSON.parse(data)
-      if (msg.music !== undefined) {
-        AudioManager.setVolume('audio', msg.music)
-      }
-      var res = {
-        type: 'Volume',
-        event: 'ON_VOLUME_CHANGE',
-        template: JSON.stringify({
-          mediaCurrent: '' + AudioManager.getVolume(AudioManager.STREAM_AUDIO),
-          mediaTotal: '100',
-          alarmCurrent: '' + AudioManager.getVolume(AudioManager.STREAM_ALARM),
-          alarmTotal: '100'
-        }),
-        appid: ''
-      }
-      logger.log('response topic set_volume ->', res)
-      mqttAgent.sendToApp('event', JSON.stringify(res))
-    })
-  }).catch((err) => {
-    logger.error(err)
-  })
-})
+      runtime.onReLogin()
 
-// var netStatus = wifi.getNetworkState();
-// if (netStatus === 3) {
-//   runtime.onEvent('disconnected', {});
-// }
+      mqttAgent.on('asr', function (asr) {
+        speechT.getNlpResult(asr, function (err, nlp, action) {
+          if (err) {
+            console.error(`occurrs some error in speechT`)
+          } else {
+            runtime.onVoiceCommand(asr, nlp, action)
+          }
+        })
+      })
+      mqttAgent.on('cloud_forward', function (data) {
+        runtime.onCloudForward(data)
+      })
+      mqttAgent.on('get_volume', function (data) {
+        var res = {
+          type: 'Volume',
+          event: 'ON_VOLUME_CHANGE',
+          template: JSON.stringify({
+            mediaCurrent: '' + AudioManager.getVolume(AudioManager.STREAM_AUDIO),
+            mediaTotal: '100',
+            alarmCurrent: '' + AudioManager.getVolume(AudioManager.STREAM_ALARM),
+            alarmTotal: '100'
+          }),
+          appid: ''
+        }
+        logger.log('response topic get_volume ->', res)
+        mqttAgent.sendToApp('event', JSON.stringify(res))
+      })
+      mqttAgent.on('set_volume', function (data) {
+        var msg = JSON.parse(data)
+        if (msg.music !== undefined) {
+          AudioManager.setVolume('audio', msg.music)
+        }
+        var res = {
+          type: 'Volume',
+          event: 'ON_VOLUME_CHANGE',
+          template: JSON.stringify({
+            mediaCurrent: '' + AudioManager.getVolume(AudioManager.STREAM_AUDIO),
+            mediaTotal: '100',
+            alarmCurrent: '' + AudioManager.getVolume(AudioManager.STREAM_ALARM),
+            alarmTotal: '100'
+          }),
+          appid: ''
+        }
+        logger.log('response topic set_volume ->', res)
+        mqttAgent.sendToApp('event', JSON.stringify(res))
+      })
+    }).catch((err) => {
+      logger.error('initializing occurrs error', err && err.stack)
+    })
+  })
+}
