75d7ca1ad1d977c79fbcd9169c8452ea562c07f1
chang.lu <chang.lu@rokid.com>
2018-09-03 14:47:58 +0800

    add tts method createTts test case
    Change-Id: I31c3050c9293bf93f00d52581fb0585592ac383f

--- test/@yoda/tts/tts.test.js
+++ test/@yoda/tts/tts.test.js
@@ -3,102 +3,198 @@
 var test = require('tape')
 var ttsModule = require('@yoda/tts')
 var logger = require('logger')('tts-test')
-// var prop = require('@yoda/property')
-// var login = require('/usr/lib/yoda/runtime/lib/cloudapi/login')
 
-// login().then((data) => {
-//   logger.info(data.deviceId)
-//   logger.info(data.deviceTypeId)
-//   logger.info(data.key)
-//   logger.info(data.secret)
-// })
-
-// var options = {
-//   key : prop.get('key'),
-//   secret : 'sds',
-//   deviceId : 'ssds',
-//   deviceTypeId : 'sdfaf'
-// }
-
-// test('module->tts->test demo', t => {
-//   t.plan(1)
-//   console.log('deeeeeeemo')
-//   t.ok(true)
-//   t.end()
-// })
-
-test('module->tts->createTts method test case: invalid options params',t => {
-  t.plan(2)
-  var tts = ttsModule.createTts({deviceId:'xxx',deviceTypeId:'xxx',key:'xxx',secret:'xxx'})
-  t.equal(typeof tts, 'object',"TtsProxy is object")
-  tts.on('error',(id, err)=>{
-    logger.error(id+" err: "+err)
-    t.ok(err!=''||err!=null)
+test.skip('module->tts->createTts method test case: normal options params', t => {
+  t.plan(3)
+  var tts = ttsModule.createTts({
+    deviceId: '0602041822000129',
+    deviceTypeId: '060F941561F24278B8ED71733D7B9507',
+    key: 'A5D4350521F84E8C859DD473E043087F',
+    secret: '658D918B788B4416AE27389D1189F5B8'
   })
-  t.end()
+  var request = tts.speak('你好若琪', cb => {
+    logger.info('call back')
+    logger.info(request)
+    t.equal(request.state, 'end', `tts : id=${id} call back`)
+  })
+  logger.info(request)
+  tts.on('start', function(id, errno) {
+    t.equal(request.state, 'start', `tts : id=${id} start`)
+  })
+  tts.on('voice', function(id, errno) {
+    logger.info('tts voice', id)
+  })
+  tts.on('end', function(id, errno) {
+    logger.info('tts end', id)
+    t.equal(request.state, 'end', `tts : id=${id} end`)
+    tts.disconnect()
+    t.end()
+  })
+  tts.on('error', function(id, errno) {
+    logger.info('tts error', id)
+  })
 })
 
-test('module->tts->createHandle method test case: error options params , get handle is null',t => {
+test.skip('module->tts->createTts method testcase :invalid options params', t => {
   t.plan(2)
-  var handle = ttsModule.createHandle({deviceId:'xxx',deviceTypeId:'xxx',key:'xxx',secret:'xxx'})
-  t.equal(typeof handle, 'object',"handle is object")
-  t.equal(Object.keys(handle).length, 0,'handle is {}')
+  var tts = ttsModule.createTts({
+    deviceId: 'xxx',
+    deviceTypeId: 'xxx',
+    key: 'xxx',
+    secret: 'xxx'
+  })
+  t.equal(typeof tts, 'object', 'TtsProxy is object')
+  tts.on('error', (id, err) => {
+    logger.error(`${id} err: ${err}`)
+    t.ok(err !== '' || err !== null)
+  })
+  setTimeout(() => {
+    logger.info('tts disconnect ...')
+    tts.disconnect()
+    t.end()
+  }, 2000)
+})
+
+test('module->tts->createHandle method test case: error options params , get handle is null', t => {
+  t.plan(2)
+  var handle = ttsModule.createHandle({
+    deviceId: 'xxx',
+    deviceTypeId: 'xxx',
+    key: 'xxx',
+    secret: 'xxx'
+  })
+  t.equal(typeof handle, 'object', 'handle is object')
+  t.equal(Object.keys(handle).length, 0, 'handle is {}')
   t.end()
 })
 
-
-test('module->tts->createHandle method test case: normal options',t => {
+test('module->tts->createHandle method test case: normal options', t => {
   t.plan(1)
-  t.doesNotThrow(()=>{
-    ttsModule.createHandle({deviceId:'xxx',deviceTypeId:'xxx',key:'xxx',secret:'xxx'})
+  t.doesNotThrow(() => {
+    ttsModule.createHandle({
+      deviceId: 'xxx',
+      deviceTypeId: 'xxx',
+      key: 'xxx',
+      secret: 'xxx'
+    })
   })
   t.end()
 })
 
-test('module->tts->createHandle method test case: error options',t => {
+test('module->tts->createHandle method test case: error options', t => {
   t.plan(14)
-  //options is null
-  t.throws(()=>{ttsModule.createHandle(null)},"options is required")
-  t.throws(()=>{ttsModule.createHandle({})},"options is required")
-  //options.deviceId is err
-  t.throws(()=>{
-    ttsModule.createHandle({deviceId: null, deviceTypeId: 'xxx', key: 'xxx', secret: 'xxx'})
-  },"options.deviceId is required")
-  t.throws(()=>{
-    ttsModule.createHandle({deviceId: '', deviceTypeId: 'xxx', key: 'xxx', secret: 'xxx'})
-  },"options.deviceId is required")
-  t.throws(()=>{
-    ttsModule.createHandle({deviceTypeId: 'xxx', key: 'xxx', secret: 'xxx'})
-  },"options.deviceId is required")
-  //options.deviceTypeId is null
-  t.throws(()=>{
-    ttsModule.createHandle({deviceId: 'xxx', deviceTypeId: null, key: 'xxx', secret: 'xxx'})
-  },"options.deviceTypeId is required")
-  t.throws(()=>{
-    ttsModule.createHandle({deviceId: 'xxx', deviceTypeId: '', key: 'xxx', secret: 'xxx'})
-  },"options.deviceTypeId is required")
-  t.throws(()=>{
-    ttsModule.createHandle({deviceId: 'xxx', key: 'xxx', secret: 'xxx'})
-  },"options.deviceTypeId is required")
-  //options.key is null
-  t.throws(()=>{
-    ttsModule.createHandle({deviceId: 'xxx', deviceTypeId: 'xxx', key: null, secret: 'xxx'})
-  },"options.key is required")
-  t.throws(()=>{
-    ttsModule.createHandle({deviceId: 'xxx', deviceTypeId: 'xxx', key: '', secret: 'xxx'})
-  },"options.key is required")
-  t.throws(()=>{
-    ttsModule.createHandle({deviceId: 'xxx', deviceTypeId: 'xxx', secret: 'xxx'})
-  },"options.key is required")
-  //options.secret is null
-  t.throws(()=>{
-    ttsModule.createHandle({deviceId: 'xxx', deviceTypeId: 'xxx', key: 'xxx', secret: null})
-  },"options.secret is required")
-  t.throws(()=>{
-    ttsModule.createHandle({deviceId: 'xxx', deviceTypeId: 'xxx', key: 'xxx', secret: ''})
-  },"options.secret is required")
-  t.throws(()=>{
-    ttsModule.createHandle({deviceId: 'xxx', deviceTypeId: 'xxx', key: 'xxx'})
-  },"options.secret is required")
+  /**
+    options is null
+    */
+  t.throws(() => {
+    ttsModule.createHandle(null)
+  }, 'options is required')
+  t.throws(() => {
+    ttsModule.createHandle({})
+  }, 'options is required')
+  /**
+    options.deviceId is err
+    */
+  t.throws(() => {
+    ttsModule.createHandle({
+      deviceId: null,
+      deviceTypeId: 'xxx',
+      key: 'xxx',
+      secret: 'xxx'
+    })
+  }, 'options.deviceId is required')
+  t.throws(() => {
+    ttsModule.createHandle({
+      deviceId: '',
+      deviceTypeId: 'xxx',
+      key: 'xxx',
+      secret: 'xxx'
+    })
+  }, 'options.deviceId is required')
+  t.throws(() => {
+    ttsModule.createHandle({
+      deviceTypeId: 'xxx',
+      key: 'xxx',
+      secret: 'xxx'
+    })
+  }, 'options.deviceId is required')
+  /**
+    options.deviceTypeId is null
+    */
+  t.throws(() => {
+    ttsModule.createHandle({
+      deviceId: 'xxx',
+      deviceTypeId: null,
+      key: 'xxx',
+      secret: 'xxx'
+    })
+  }, 'options.deviceTypeId is required')
+  t.throws(() => {
+    ttsModule.createHandle({
+      deviceId: 'xxx',
+      deviceTypeId: '',
+      key: 'xxx',
+      secret: 'xxx'
+    })
+  }, 'options.deviceTypeId is required')
+  t.throws(() => {
+    ttsModule.createHandle({
+      deviceId: 'xxx',
+      key: 'xxx',
+      secret: 'xxx'
+    })
+  }, 'options.deviceTypeId is required')
+  /**
+    options.key is null
+    */
+  t.throws(() => {
+    ttsModule.createHandle({
+      deviceId: 'xxx',
+      deviceTypeId: 'xxx',
+      key: null,
+      secret: 'xxx'
+    })
+  }, 'options.key is required')
+  t.throws(() => {
+    ttsModule.createHandle({
+      deviceId: 'xxx',
+      deviceTypeId: 'xxx',
+      key: '',
+      secret: 'xxx'
+    })
+  }, 'options.key is required')
+  t.throws(() => {
+    ttsModule.createHandle({
+      deviceId: 'xxx',
+      deviceTypeId: 'xxx',
+      secret: 'xxx'
+    })
+  }, 'options.key is required')
+  /**
+    options.secret is null
+    */
+  t.throws(() => {
+    ttsModule.createHandle({
+      deviceId: 'xxx',
+      deviceTypeId: 'xxx',
+      key: 'xxx',
+      secret: null
+    })
+  }, 'options.secret is required')
+  t.throws(() => {
+    ttsModule.createHandle({
+      deviceId: 'xxx',
+      deviceTypeId: 'xxx',
+      key: 'xxx',
+      secret: ''
+    })
+  }, 'options.secret is required')
+  t.throws(() => {
+    ttsModule.createHandle({
+      deviceId: 'xxx',
+      deviceTypeId: 'xxx',
+      key: 'xxx'
+    })
+  }, 'options.secret is required')
   t.end()
 })
