10fdb505e1ac6cf81877c49b746ba94319bc1689
xuxuechao <xxc@xuxuechaodeMacBook-Pro.local>
2018-09-03 15:01:11 +0800

    added audio/media unitest
    Change-Id: I26c38335c4ee4f3182346d5a60fed52108e67ac9

--- test/@yoda/multimedia/simple.test.js
+++ test/@yoda/multimedia/simple.test.js
@@ -3,19 +3,24 @@
 var test = require('tape')
 var MediaPlayer = require('@yoda/multimedia').MediaPlayer
 var AudioManager = require('@yoda/audio').AudioManager
+var logger = require('logger')('simple.test')
 
-test.skip('should play a wakeup music', t => {
+test('should play a wakeup music', t => {
   t.plan(3)
   var player = new MediaPlayer()
   t.equal(player._stream, AudioManager.STREAM_PLAYBACK)
-
-  player.start('/opt/media/wakeup.ogg')
+  logger.log("test sss***")
+  
   player.on('prepared', () => {
+    logger.log("test pass***")
     t.pass()
   })
   player.on('playbackcomplete', () => {
+    logger.log("test sssss***")
     player.stop()
     t.pass()
     t.end()
   })
+  t.player.playing
+  player.start('/opt/media/wakeup.ogg')
 })
