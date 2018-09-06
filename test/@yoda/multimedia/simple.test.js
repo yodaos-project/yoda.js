3f7c1487edbf1d6a667c687fa9d3470cea03b023
Yorkie Liu <yorkiefixer@gmail.com>
2018-09-06 18:39:45 +0800

    wifi: describe how the joinNetwork does
    Fixes: https://bug2b.rokid-inc.com/zentao/bug-view-1308.html
    Change-Id: Ibeabfc1a8e02cebb6f765a92bf942b7f39b44baa

--- test/@yoda/multimedia/simple.test.js
+++ test/@yoda/multimedia/simple.test.js
@@ -5,7 +5,7 @@ var MediaPlayer = require('@yoda/multimedia').MediaPlayer
 var AudioManager = require('@yoda/audio').AudioManager
 var logger = require('logger')('simple.test')
 
-test('should play a wakeup music', t => {
+test('should play a wakeup music', (t) => {
   t.plan(3)
   var player = new MediaPlayer()
   t.equal(player._stream, AudioManager.STREAM_PLAYBACK)
