7e6139b1d8e2b530abccd83e681d1027dd107f94
LanFly <1260022720@qq.com>
2018-09-07 17:08:44 +0800

    fixed setVolume light and add button type
    Change-Id: I362e0654a16468e2b17fad10904b3dc8ac2bd5b8

--- res/light/setVolume.js
+++ res/light/setVolume.js
@@ -3,18 +3,35 @@
 module.exports = function (light, data, callback) {
   var pos = Math.floor((data.volume / 100) * light.ledsConfig.leds)
   pos = Math.max(1, pos)
+
+  function delayAndShutdown () {
+    light.requestAnimationFrame(() => {
+      light.transition({ r: 255, g: 255, b: 255 }, { r: 0, g: 0, b: 0 }, 130, 4, (r, g, b, lastFrame) => {
+        for (var i = 0; i < pos; i++) {
+          light.pixel(i, r, g, b)
+        }
+        light.render()
+        if (lastFrame) {
+          callback()
+        }
+      })
+    }, 1000)
+  }
+
   var player = light.sound('system://volume.wav')
 
   for (var i = 0; i < pos; i++) {
     light.pixel(i, 255, 255, 255)
   }
   light.render()
+  delayAndShutdown()
+
   callback()
 
   return {
     stop: function () {
       player.stop()
-      light.stop()
+      light.stop(true)
     }
   }
 }
