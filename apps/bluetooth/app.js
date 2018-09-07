05afdc7d2bc868774a3a84f9e16dcc385a70bc48
LanFly <1260022720@qq.com>
2018-09-08 01:19:44 +0800

    fixed format and add permission
    Change-Id: Id417ec63a46a7f7f6ce8270bba27a1f46dccff94

--- apps/bluetooth/app.js
+++ apps/bluetooth/app.js
@@ -1,41 +1,102 @@
 'use strict'
 
-var bluetooth = require('bluetooth')
-
+var bluetooth = require('@yoda/bluetooth')
+var logger = require('logger')('eventReq')
+var property = require('@yoda/property')
 module.exports = function (activity) {
-  var agent = null
   var player = null
-  var name = 'Rokid-Me-072'
+  var start = null
+  var uuid = property.get('ro.boot.serialno') || ''
+  var name = 'Rokid-Me-' + uuid.substr(-6)
   var STRING_BROADCAST = '蓝牙已打开，请使用手机搜索设备'
+  var STRING_CONNECED = '连接蓝牙成功'
   var STRING_CLOSED = '蓝牙已关闭'
 
   function broadcast () {
-    speakAndExit(STRING_BROADCAST + name)
-    if (agent === null) {
-      agent = bluetooth.getBluetooth(name)
+    player = bluetooth.getPlayer()
+    setTimeout(() => {
+      start = player.start(name)
+      if (start) {
+        speakAndExit(STRING_BROADCAST + name)
+      }
+    }, 1000)
+    player.on('stateupdate', function (message) {
+      if (message.connect_name && message.connect_state === 'connected') {
+        activity.setForeground()
+          .then(() => {
+            speakAndExit(STRING_CONNECED)
+          })
+      }
     }
-    if (player === null) {
-      player = agent.createPlayer()
-      player.on('open', () => {
-        // auto play when open
-        player.play()
-      })
-    }
+    )
   }
 
   function disconnect () {
-    speakAndExit(STRING_CLOSED)
-    if (agent) {
-      // TODO
-      agent = null
-      player = null
+    if (player) {
+      player.end()
+      player.disconnect()
+    }
+    activity.tts.speak(STRING_CLOSED)
+      .then(() => {
+        activity.exit()
+      })
+  }
+
+  function startMusic () {
+    player = bluetooth.getPlayer()
+    if (player) {
+      player.play()
     }
   }
 
+  function pauseMusic () {
+    player = bluetooth.getPlayer()
+    if (player) {
+      player.pause()
+    }
+  }
+  /**
+  function resumeMusic () {
+    player = bluetooth.getPlayer()
+    if (player) {
+      player.resume()
+    }
+  }
+
+  function nextMusic () {
+    player = bluetooth.getPlayer()
+    if (player) {
+      player.next()
+    }
+  }
+
+  function previousMusic () {
+    player = bluetooth.getPlayer()
+    if (player) {
+      player.previous()
+    }
+  }
+  */
+
   function speakAndExit (text) {
     return activity.tts.speak(text)
-      .then(() => activity.exit())
+      .then(() => activity.setBackground())
   }
+
+  activity.on('pause', () => {
+    pauseMusic()
+  })
+  activity.on('destroy', () => {
+    pauseMusic()
+    player.disconnect()
+    activity.exit()
+    speakAndExit(STRING_CLOSED)
+  })
+
+  activity.on('resume', () => {
+    logger.log('bluetooth music is resume')
+    startMusic()
+  })
 
   activity.on('request', function (nlp, action) {
     switch (nlp.intent) {
@@ -45,6 +106,22 @@ module.exports = function (activity) {
       case 'bluetooth_disconnect':
         disconnect()
         break
+      case 'play_bluetoothmusic':
+        logger.log('play_bluetoothmusic music is startmusic')
+        startMusic()
+        break
+        /* case 'next':
+            nextMusic()
+            break
+          case 'previous':
+            previousMusic()
+            break
+          case 'stop':
+            pauseMusic()
+            break
+          case 'resume':
+            resumeMusic()
+            break */
       default:
         activity.exit()
         break
