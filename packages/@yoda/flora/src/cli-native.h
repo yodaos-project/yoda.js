e2f4d74cef111f09756071bee813d6429d77f895
ming <chen.zhang@rokid.com>
2018-09-08 14:20:39 +0800

    tab to spaces
    Change-Id: I87529cfe0a217b2a90dfd73bfdf93619fbe5c4ce

--- packages/@yoda/flora/src/cli-native.h
+++ packages/@yoda/flora/src/cli-native.h
@@ -17,24 +17,24 @@ extern "C" {
 class NativeCallback;
 
 typedef struct {
-	// 0: recv_post
-	// 1: disconnected
-	int32_t cb_type;
-	std::string name;
-	uint32_t msgtype;
-	std::shared_ptr<Caps> msg;
+  // 0: recv_post
+  // 1: disconnected
+  int32_t cb_type;
+  std::string name;
+  uint32_t msgtype;
+  std::shared_ptr<Caps> msg;
 } AsyncCallbackInfo;
 
 typedef struct {
-	std::shared_ptr<flora::Client> cli;
-	std::list<AsyncCallbackInfo> pending_callbacks;
-	std::mutex cb_mutex;
+  std::shared_ptr<flora::Client> cli;
+  std::list<AsyncCallbackInfo> pending_callbacks;
+  std::mutex cb_mutex;
 } flora_cli_stl;
 
 typedef struct {
-	iotjs_jobjectwrap_t jobjectwrap;
-	NativeCallback* callback;
-	uv_async_t async;
-	// IOTJS_ALLOC分配的内存全部清零，不可直接定义stl结构体
-	flora_cli_stl* stl_st;
+  iotjs_jobjectwrap_t jobjectwrap;
+  NativeCallback* callback;
+  uv_async_t async;
+  // IOTJS_ALLOC分配的内存全部清零，不可直接定义stl结构体
+  flora_cli_stl* stl_st;
 } IOTJS_VALIDATED_STRUCT(iotjs_flora_cli_t);
