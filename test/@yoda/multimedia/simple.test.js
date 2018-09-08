d64d32407d081a9d2d64ce3752ed04ddf26bdca9
Yorkie Liu <yorkiefixer@gmail.com>
2018-09-08 18:44:28 +0800

    test: remove unused logger
    Change-Id: I72d8b81b93706c4309cf3a69d4c6c221c5132f30

--- test/@yoda/multimedia/simple.test.js
+++ test/@yoda/multimedia/simple.test.js
@@ -3,7 +3,6 @@
 var test = require('tape')
 var MediaPlayer = require('@yoda/multimedia').MediaPlayer
 var AudioManager = require('@yoda/audio').AudioManager
-var logger = require('logger')('simple.test')
 
 test('should play a wakeup music', (t) => {
   t.plan(3)
