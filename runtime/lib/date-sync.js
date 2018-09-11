301b0acea0ca0512fcb5007074d285f8c7927831
Yorkie Liu <yorkiefixer@gmail.com>
2018-09-12 00:38:25 +0800

    runtime: sync date from server when login
    Change-Id: I08fa2ff9584ce20980481b492e7dcb2591175291

---
+++ runtime/lib/date-sync.js
@@ -0,0 +1,25 @@
+'use strict'
+
+var exec = require('child_process').exec
+var parseDateString = require('@yoda/system').parseDateString
+var logger = require('logger')('date-sync')
+
+var TIMEZONE = 8
+var DATE_FORMAT = '%A, %d %b %Y %H:%M:%S'
+
+function sync (source) {
+  var date = parseDateString(source, DATE_FORMAT)
+  if (date.hours + TIMEZONE >= 24) {
+    date.date += 1
+    date.hours = date.hours + TIMEZONE - 24
+  }
+  var str = [
+    [date.year, date.month, date.date].join('-'),
+    [date.hours, date.minutes, date.seconds].join(':'),
+  ].join(' ')
+
+  var cmd = `date -s "${str}"`
+  logger.log(`exec ${cmd}`)
+  exec(cmd)
+}
+exports.sync = sync
