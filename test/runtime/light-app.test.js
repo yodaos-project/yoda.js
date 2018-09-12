bc921e156f2daf5dd9f817d0980399dbc60198c3
Wu Chengzhong <chengzhong.wu@rokid.com>
2018-09-12 16:32:23 +0800

    test(light-app): light app now returns promise
    Change-Id: I85f68d244fa0575a213da0e79e4313a6f6b9fc73

--- test/runtime/light-app.test.js
+++ test/runtime/light-app.test.js
@@ -14,50 +14,61 @@ var ActivityDescriptor = Descriptors.ActivityDescriptor
 var MultimediaDescriptor = Descriptors.MultimediaDescriptor
 
 test('should listen events', t => {
+  proxy.removeAllListeners()
   var runtime = new EventEmitter()
 
-  var descriptor = lightApp('@test', target, runtime)
+  lightApp('@test', target, runtime)
+    .then(descriptor => {
+      var activityEvents = Object.keys(ActivityDescriptor.prototype).filter(key => {
+        var desc = ActivityDescriptor.prototype[key]
+        return desc.type === 'event'
+      })
+      var multimediaEvents = Object.keys(MultimediaDescriptor.prototype).filter(key => {
+        var desc = MultimediaDescriptor.prototype[key]
+        return desc.type === 'event'
+      })
 
-  var activityEvents = Object.keys(ActivityDescriptor.prototype).filter(key => {
-    var desc = ActivityDescriptor.prototype[key]
-    return desc.type === 'event'
-  })
-  var multimediaEvents = Object.keys(MultimediaDescriptor.prototype).filter(key => {
-    var desc = MultimediaDescriptor.prototype[key]
-    return desc.type === 'event'
-  })
+      activityEvents.forEach(it => {
+        t.assert(descriptor.listeners(it).length > 0, `event '${it}' should have been listened.`)
+      })
+      multimediaEvents.forEach(it => {
+        t.assert(descriptor.media.listeners(it).length > 0, `media event '${it}' should have been listened.`)
+      })
 
-  activityEvents.forEach(it => {
-    t.assert(descriptor.listeners(it).length > 0, `event '${it}' should have been listened.`)
-  })
-  multimediaEvents.forEach(it => {
-    t.assert(descriptor.media.listeners(it).length > 0, `media event '${it}' should have been listened.`)
-  })
-
-  t.end()
-  proxy.removeAllListeners()
+      t.end()
+    })
+    .catch(err => {
+      t.error(err)
+      t.end()
+    })
 })
 
 test('should subscribe event-ack', t => {
-  var runtime = new EventEmitter()
-  var descriptor = lightApp('@test', target, runtime)
-
-  var activityEvents = Object.keys(ActivityDescriptor.prototype).filter(key => {
-    var desc = ActivityDescriptor.prototype[key]
-    return desc.type === 'event-ack'
-  })
-
-  activityEvents.forEach(it => {
-    var eventDescriptor = descriptor[it]
-    t.strictEqual(typeof descriptor[eventDescriptor.trigger], 'function',
-      `event-ack '${it}' should have been subscribed.`)
-  })
-
-  t.end()
   proxy.removeAllListeners()
+  var runtime = new EventEmitter()
+  lightApp('@test', target, runtime)
+    .then(descriptor => {
+      var activityEvents = Object.keys(ActivityDescriptor.prototype).filter(key => {
+        var desc = ActivityDescriptor.prototype[key]
+        return desc.type === 'event-ack'
+      })
+
+      activityEvents.forEach(it => {
+        var eventDescriptor = descriptor[it]
+        t.strictEqual(typeof descriptor[eventDescriptor.trigger], 'function',
+          `event-ack '${it}' should have been subscribed.`)
+      })
+
+      t.end()
+    })
+    .catch(err => {
+      t.error(err)
+      t.end()
+    })
 })
 
 test('should receive life cycle events', t => {
+  proxy.removeAllListeners()
   t.plan(6)
   proxy.on('create', () => {
     t.pass('event `created` received')
@@ -78,50 +89,63 @@ test('should receive life cycle events', t => {
     t.strictEqual(gotNlp, nlp, 'nlp comparison')
     t.strictEqual(gotAction, action, 'action comparison')
   })
-  var app = lightApp('@test', target, {})
-  app.emit('create')
-  app.emit('pause')
-  app.emit('resume')
-  app.emit('destroy')
-  app.emit('request', nlp, action)
+  lightApp('@test', target, {})
+    .then(app => {
+      app.emit('create')
+      app.emit('pause')
+      app.emit('resume')
+      app.emit('destroy')
+      app.emit('request', nlp, action)
 
-  t.end()
-  proxy.removeAllListeners()
+      t.end()
+    })
+    .catch(err => {
+      t.error(err)
+      t.end()
+    })
 })
 
 test('should populate methods', t => {
-  var app = lightApp('@test', target, {})
-  var activity = app.activity
-
-  var activityMethods = [ 'destroyAll', 'exit', 'get',
-    'playSound', 'setBackground', 'setConfirm', 'setForeground', 'setPickup' ]
-  activityMethods.forEach(it => {
-    t.strictEqual(typeof activity[it], 'function', `activity.${it} has to be a function`)
-  })
-
-  var namespaces = {
-    light: [ 'play', 'stop' ],
-    media: [ 'getLoopMode', 'getPosition', 'pause', 'resume', 'seek',
-      'setLoopMode', 'start', 'stop' ],
-    tts: [ 'speak', 'stop' ]
-  }
-  Object.keys(namespaces).forEach(key => {
-    var ns = namespaces[key]
-    t.strictEqual(typeof activity[key], 'object', `activity.${key} has to be an object`)
-    ns.forEach(it => {
-      t.strictEqual(typeof activity[key][it], 'function', `activity.${key}.${it} has to be a function`)
-    })
-  })
-
-  t.end()
   proxy.removeAllListeners()
+  lightApp('@test', target, {})
+    .then(app => {
+      var activity = app.activity
+
+      var activityMethods = [ 'destroyAll', 'exit', 'get',
+        'playSound', 'setBackground', 'setConfirm', 'setForeground', 'setPickup' ]
+      activityMethods.forEach(it => {
+        t.strictEqual(typeof activity[it], 'function', `activity.${it} has to be a function`)
+      })
+
+      var namespaces = {
+        light: [ 'play', 'stop' ],
+        media: [ 'getLoopMode', 'getPosition', 'pause', 'resume', 'seek',
+          'setLoopMode', 'start', 'stop' ],
+        tts: [ 'speak', 'stop' ]
+      }
+      Object.keys(namespaces).forEach(key => {
+        var ns = namespaces[key]
+        t.strictEqual(typeof activity[key], 'object', `activity.${key} has to be an object`)
+        ns.forEach(it => {
+          t.strictEqual(typeof activity[key][it], 'function', `activity.${key}.${it} has to be a function`)
+        })
+      })
+
+      t.end()
+    })
+    .catch(err => {
+      t.error(err)
+      t.end()
+    })
 })
 
 test('should populate direct values', t => {
-  var app = lightApp('@test', target, {})
-  var activity = app.activity
-  t.strictEqual(activity.appId, '@test')
-  t.strictEqual(activity.appHome, target)
-  t.end()
   proxy.removeAllListeners()
+  lightApp('@test', target, {})
+    .then(app => {
+      var activity = app.activity
+      t.strictEqual(activity.appId, '@test')
+      t.strictEqual(activity.appHome, target)
+      t.end()
+    })
 })
