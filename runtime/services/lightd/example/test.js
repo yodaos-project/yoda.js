00eb52cf08ea2bbe5adabf8c98666b6ca6932984
LanFly <1260022720@qq.com>
2018-09-12 17:00:40 +0800

    lightd: change from single instance to multiple instances
    Change-Id: I1229a4a4f6c25c0e49ee4453bd3754ed0ed38dc1

--- runtime/services/lightd/example/test.js
+++ runtime/services/lightd/example/test.js
@@ -1,36 +1,8 @@
 var Service = require('../service')
 var Light = require('@yoda/light')
-var MediaPlayer = require('@yoda/multimedia').MediaPlayer
-var Effects = require('../effects')
 
 console.log(JSON.stringify(Light.getProfile()))
 
-var effect = new Effects(Light, MediaPlayer)
+var light = new Service()
 
-var light = new Service({
-  effect: effect
-})
-
-light.setStandby()
-
-// effect.requestAnimationFrame(() => {
-//   light.setAwake();
-//   effect.requestAnimationFrame(() => {
-//     light.setDegree(120);
-//     effect.requestAnimationFrame(() => {
-//       light.setLoading(120);
-//       effect.requestAnimationFrame(() => {
-//         light.setVolume(10);
-//         effect.requestAnimationFrame(() => {
-//           light.setVolume(20);
-//           effect.requestAnimationFrame(() => {
-//             light.setVolume(30);
-//             effect.requestAnimationFrame(() => {
-//               light.setWelcome();
-//             }, 2000);
-//           }, 1000);
-//         }, 1000);
-//       }, 4000);
-//     }, 2000);
-//   }, 2000);
-// }, 4000);
+light.setWelcome()
