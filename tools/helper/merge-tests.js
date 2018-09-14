77235e79a6b16adf12f18a79a3a4f6a1396b9b18
Yorkie Liu <yorkiefixer@gmail.com>
2018-09-14 21:59:20 +0800

    tools: add testci for ci script
    Change-Id: Ia69a99470e1b0eed4219fb7306c818398be913b0

---
+++ tools/helper/merge-tests.js
@@ -0,0 +1,32 @@
+'use strict'
+
+var fs = require('fs')
+var path = require('path')
+var TapParser = require('tap-parser')
+var resultRoot = path.join(__dirname, '../../test/.result')
+
+var result = fs.readdirSync(resultRoot).reduce((result, filename) => {
+  var parser = new TapParser()
+  var currTest
+  parser.on('comment', (msg) => {
+    msg = msg.replace(/^\# /, '').replace('\n', '')
+    currTest = result.tests[msg] = []
+  })
+  parser.on('assert', (data) => {
+    result.total += 1
+    data.ok ? result.pass += 1 : result.fail += 1
+    if (currTest) {
+      currTest.push({ ok: data.ok, name: data.name })
+    }
+  })
+  var contents = fs.readFileSync(`${resultRoot}/${filename}`)
+  parser.write(contents)
+  return result
+}, {
+  pass: 0,
+  fail: 0,
+  total: 0,
+  tests: {}
+})
+
+console.log(JSON.stringify(result, null, 2))
