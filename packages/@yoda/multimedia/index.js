c9b7380333a8242284f63f7ad7311f0d92ac8464
Yorkie Liu <yorkiefixer@gmail.com>
2018-09-27 16:43:14 +0800

    multimeida: fix lint errors
    Change-Id: I13ecc6744e60e59e05391afbc13183968a06b301

--- packages/@yoda/multimedia/index.js
+++ packages/@yoda/multimedia/index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 /**
- * The multimedia includes `MediaPlayer` and `Sounder`. mediaplayer support for playing 
+ * The multimedia includes `MediaPlayer` and `Sounder`. mediaplayer support for playing
  * variety of common media types, so that you can easily integrate audio into your applications.
  * `Sounder` is a simple player, support for playing wav type, for faster playback.
  * @module @yoda/multimedia
