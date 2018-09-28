45339735d09f986a97e6368955c06b9cbe090694
Yorkie Liu <yorkiefixer@gmail.com>
2018-09-28 13:14:30 +0800

    tools: add upgrade script
    Change-Id: I4d668b0ae1a23b2b53394fc2a95532c8495a1871

---
+++ tools/helper/upgrade.js
@@ -0,0 +1,12 @@
+'use strict'
+
+var fs = require('fs')
+var system = require('@yoda/system')
+var imgPathname = process.argv[2]
+
+if (fs.existsSync(imgPathname) === false) {
+  throw new Error(`${imgPathname} not exists`)
+}
+
+system.prepareOta(imgPathname)
+system.reboot()
