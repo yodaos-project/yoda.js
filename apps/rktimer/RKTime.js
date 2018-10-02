5dc3c704c680df1a2da271e3dc8982d408efa43e
medea <medea@rokid.com>
2018-10-02 13:06:55 +0800

    timer app code version: 0.9
    Change-Id: I3ea3e8e9244303bc4c50bf6e3773166f48648cbe

---
+++ apps/rktimer/RKTime.js
@@ -0,0 +1,25 @@
+'use strict'
+
+function RKTime (second, minute, hour, day) {
+  this.day = day
+  this.hour = hour
+  this.minute = minute
+  this.second = second
+}
+
+RKTime.prototype.setSecond = function (second) {
+  this.day = second / 86400
+  this.hour = (second % 86400) / 3600
+  this.minute = (second % 86400 % 3600) / 60
+  this.second = second % 86400 % 3600 % 60
+}
+
+RKTime.prototype.getSecond = function () {
+  return this.day * 86400 + this.hour * 3600 + this.minute * 60 + this.second
+}
+
+RKTime.prototype.toString = function () {
+  return 'RKTime'
+}
+
+exports.RKTime = RKTime
