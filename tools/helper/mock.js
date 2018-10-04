8ef3f0caca7070b18355d8b48245ab6f7a836944
Wu Chengzhong <chengzhong.wu@rokid.com>
2018-10-04 22:13:46 +0800

    tools(mock): mock keyboard events
    Change-Id: I4e7a2255d6869fbb7ef76aa8d91fb0a0db8ed1ac

---
+++ tools/helper/mock.js
@@ -0,0 +1,71 @@
+var dbus = require('dbus').getBus('session')
+var _ = require('@yoda/util')._
+var compose = require('@yoda/util').compose
+
+var type = process.argv[2]
+var verbose = process.argv[3]
+var arg = process.argv[4]
+
+var map = {
+  asr: 'mockAsr',
+  key: 'mockKeyboard'
+}
+
+main()
+
+function main () {
+  var method = map[type]
+  if (method == null) {
+    console.error('Unknown mock:', type)
+    return process.exit(1)
+  }
+
+  console.log(`Mocking ${type}:`, arg)
+  compose([
+    cb => {
+      dbus.getInterface(
+        'com.rokid.AmsExport',
+        '/rokid/yoda/debug',
+        'rokid.yoda.Debug',
+        cb
+      )
+    },
+    (cb, iface) => {
+      if (iface == null) {
+        console.error('Error: VuiDaemon is not ready, try again later.')
+        return process.exit(1)
+      }
+      if (typeof iface[method] !== 'function') {
+        console.error(`Error: '${method}' is not implemented in YodaRT, try install a newer version of YodaRT and try again.`)
+        return process.exit(1)
+      }
+      var callback = _.once(cb)
+      iface[method](arg, callback)
+      // TODO: timer shall be added once timer is ready on ShadowNode
+    }
+  ], function onDone (err, result) {
+    if (err) {
+      console.error('Unexpected error on mock:', err && err.message)
+      console.error(err && err.stack)
+      return process.exit(1)
+    }
+    var data
+    try {
+      data = JSON.parse(result)
+    } catch (err) {
+      console.error('Failed to parse result from VuiDaemon:', result)
+    }
+    console.log('Ok:', data.ok === true)
+    if (verbose !== 'YES') {
+      return process.exit(data.ok === true ? 0 : 1)
+    }
+    if (data.ok !== true) {
+      console.log('Error:', data.message)
+      console.log('Stack:', data.stack)
+      return process.exit(1)
+    }
+    console.log('Result:', JSON.stringify(data.result, null, 2))
+
+    process.exit(0)
+  })
+}
