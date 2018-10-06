913cbf792bfefc8d303436bc8ad8d31771f8ac65
Yorkie Liu <yorkiefixer@gmail.com>
2018-10-06 15:21:32 +0800

    mqtt: optimize the log and tweak the reconnect logic
    Check the handle if marked as null after register callback is done. And
    adds a timeout(2s) to reconnect for reducing CPU load.
    
    Fixes: https://bug.rokid-inc.com/zentaopms/www/index.php?m=bug&f=view&bugID=15133
    Change-Id: I706eb868c329bbc8932923b6590a05c9f9146495

--- runtime/lib/cloudapi/mqtt.js
+++ runtime/lib/cloudapi/mqtt.js
@@ -7,41 +7,81 @@ var mqttRegister = require('./mqtt-register')
 var logger = require('logger')('mqtt')
 var env = require('../env')()
 
+var reconnectTimeout = 2000
 var endpoint = env.mqtt.uri
 var handle = null
 
+/**
+ * @class
+ * @auguments EventEmitter
+ * @param {object} config -
+ * @param {string} config.masterId - the `masterId`.
+ * @param {string} config.deviceId - the `deviceId`.
+ * @param {string} config.key - the cloud key.
+ * @param {string} config.secret - the cloud secret.
+ */
 function MqttAgent (config) {
   EventEmitter.call(this)
   this.config = config
+  this.reconnecting = false
+  this.reconnectTimer = null
+  this.mqttOptions = null
   this.initialize()
 }
 inherits(MqttAgent, EventEmitter)
 
+/**
+ * Initialize the MQTT connecting which includes:
+ * - disconnect if current has connected.
+ * - start registering the MQTT token.
+ * - connect the MQTT.
+ *
+ * Note that if handle is marked as disconnected(null), it will
+ * skip the time of initializing.
+ */
 MqttAgent.prototype.initialize = function initialize () {
-  this.disconnect()
+  logger.info('start initializing the mqtt')
+  this.disconnect() // this clears the `reconnectTimer` and `reconnecting`.
   this.register((err) => {
-    if (err) {
-      logger.error('register error with error', err && err.stack)
-      if (handle !== null) {
-        this.initialize()
-      }
+    if (handle === null) {
+      logger.warn('skip connect because the handle has been flagged as null')
       return
     }
-    this.connect()
+    if (err) {
+      logger.error('register error', err && err.stack)
+      this.reconnect()
+    } else {
+      logger.info('register succssfully and start connecting.')
+      this.connect()
+    }
   })
 }
 
+/**
+ * Fetch the token by the auth config.
+ * @param {function} cb -
+ */
 MqttAgent.prototype.register = function register (cb) {
   mqttRegister.registry(this.config, (err, data) => {
     if (err) {
       return cb(err)
     }
     this.mqttOptions = data
-    cb(null)
+    cb(null, data)
   })
 }
 
+/**
+ * Connect the MQTT broker. If the connection has been established, it disconnects
+ * in subsequent. This function must be called within an un-null `mqttOptions` by
+ * `register()`.
+ *
+ * @throws {Error} call `register()` is required.
+ */
 MqttAgent.prototype.connect = function connect () {
+  if (this.mqttOptions === null) {
+    throw new Error('call `register()` is required.')
+  }
   if (handle != null) {
     this.disconnect()
   }
@@ -61,33 +101,61 @@ MqttAgent.prototype.connect = function connect () {
     logger.info('subscribed', channelId)
   })
   handle.on('offline', () => {
-    logger.error(`offline, reconnecting`)
-    this.initialize()
+    logger.error(`mqtt is offline`)
+    this.reconnect()
   })
   handle.on('message', this.onMessage.bind(this))
   handle.on('error', (err) => {
-    logger.error(`MQTT connecting error(${err && err.stack})`)
-    this.initialize()
+    logger.error(`mqtt occurs an error`, err && err.stack)
+    this.reconnect()
   })
 }
 
+/**
+ * Reconnect MQTT connects with `reconnectTimeout`.
+ */
+MqttAgent.prototype.reconnect = function reconnect () {
+  if (this.reconnecting === true) {
+    logger.warn('mqtt is reconnecting, just skip')
+    return
+  }
+  this.reconnecting = true
+  logger.info(`starts to reconnect after ${reconnectTimeout}ms`)
+  this.reconnectTimer = setTimeout(() => this.initialize(), reconnectTimeout)
+}
+
+/**
+ * Disconnect the current connection and mark the global variable as a null value.
+ */
 MqttAgent.prototype.disconnect = function disconnect () {
+  // clear the reconnect-related handles
+  this.mqttOptions = null
+  this.reconnecting = false
+  clearTimeout(this.reconnectTimer)
+
+  // clear handle
   if (handle) {
+    logger.info('disconnecting the mqtt and set the handle to null')
     handle.disconnect()
     handle.removeAllListeners()
     handle = null
+  } else {
+    logger.info('just skip disconnect, because the handle has already been disconnected')
   }
 }
 
+/**
+ * Handle the message
+ * @param {string} channel - the channel.
+ * @param {Buffer} message - the receving message.
+ */
 MqttAgent.prototype.onMessage = function onMessage (channel, message) {
   var msg
   try {
     msg = JSON.parse(message + '')
   } catch (err) {
-    msg = {}
+    logger.error(`parse error with message: ${channel} ${message}`)
     logger.error(err && err.stack)
-    logger.error('parse error with message: ', channel, message + '')
-    logger.error(message.toString('hex'))
     return
   }
   logger.info(`mqtt message with topic -> ${msg.topic}; text -> ${msg.text}`)
@@ -98,14 +166,21 @@ MqttAgent.prototype.onMessage = function onMessage (channel, message) {
   }
 }
 
+/**
+ * Send the topic the app channel.
+ * @param {string} topic
+ * @param {Buffer} text
+ * @throws {Error} mqtt is not connected yet.
+ */
 MqttAgent.prototype.sendToApp = function sendToApp (topic, text) {
   if (handle == null) {
-    throw new Error('mqtt is not connected yet')
+    throw new Error('mqtt is not connected yet.')
   }
-  logger.info('mqtt send channel:', `u/${this.config.masterId}/rc`)
-  handle.publish(`u/${this.config.masterId}/rc`, JSON.stringify({
+  var masterId = this.config.masterId
+  logger.info('mqtt send channel:', `u/${masterId}/rc`)
+  handle.publish(`u/${masterId}/rc`, JSON.stringify({
     reviceDevice: {
-      accountId: this.config.masterId
+      accountId: masterId
     },
     sourceDevice: {
       deviceId: this.config.deviceId,
@@ -115,7 +190,7 @@ MqttAgent.prototype.sendToApp = function sendToApp (topic, text) {
     text: text,
     messageId: Date.now() + ''
   }))
-  logger.info('mqtt send message to app with topic ->', topic, '; text ->', text)
+  logger.info(`mqtt send message to app with topic -> ${topic}; text -> ${text}`)
 }
 
 module.exports = MqttAgent
