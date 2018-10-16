58ddbe70edcc9077ff3845f893dfe4cc891f07c4
Yorkie Liu <yorkiefixer@gmail.com>
2018-10-16 20:14:37 +0800

    res: fix mute and volume effects and add firstboot media
    Change-Id: Ia2c510cc63fd56d6f2f20de89ac559bc4d865931

--- res/light/setMuted.js
+++ res/light/setMuted.js
@@ -2,31 +2,15 @@
 
 module.exports = function (light, data, callback) {
   var muted = !!(data && data.muted)
-  var leds = light.ledsConfig.leds
   light.clear()
-  var player
 
   if (!muted) {
-    player = light.sound(`system://mic_enable.ogg`)
+    light.sound('system://mic_enable.ogg')
     light.render()
-    return {
-      stop: () => {
-        callback()
-        player.stop()
-        light.stop()
-      }
-    }
-  }
-
-  player = light.sound('system://mic_close_tts.ogg')
-  light.pixel(leds - 1, 255, 0, 0)
-  light.render()
-  callback()
-  return {
-    stop: () => {
-      callback()
-      player.stop()
-      light.stop(true)
-    }
+    callback()
+  } else {
+    light.sound('system://mic_close_tts.ogg')
+    light.fill(255, 0, 0)
+    light.render()
   }
 }
--- res/light/setVolume.js
+++ res/light/setVolume.js
@@ -4,35 +4,22 @@ module.exports = function (light, data, callback) {
   var pos = Math.floor((data.volume / 100) * light.ledsConfig.leds)
   pos = Math.max(1, pos)
 
-  function delayAndShutdown () {
-    light.requestAnimationFrame(() => {
-      light.transition({ r: 255, g: 255, b: 255 }, { r: 0, g: 0, b: 0 }, 130, 26, (r, g, b, lastFrame) => {
-        for (var i = 0; i < pos; i++) {
-          light.pixel(i, r, g, b)
-        }
-        light.render()
-        if (lastFrame) {
-          callback()
-        }
-      })
-    }, 1000)
-  }
+  var from = { r: 255, g: 255, b: 255 }
+  var to = { r: 0, g: 0, b: 0 }
 
-  var player = light.sound('system://volume.wav')
-
-  light.fill(0, 0, 0)
-  for (var i = 0; i < pos; i++) {
-    light.pixel(i, 255, 255, 255)
-  }
-  light.render()
-  delayAndShutdown()
-
-  callback()
-
-  return {
-    stop: function () {
-      player.stop()
-      light.stop(true)
+  function render (r, g, b) {
+    for (var i = 0; i < pos; i++) {
+      light.pixel(i, r, g, b)
     }
+    light.render()
   }
+
+  light.sound('system://volume.wav')
+  light.fill(0, 0, 0)
+
+  render(from.r, from.g, from.b)
+  light.requestAnimationFrame(() => {
+    light.transition(from, to, 130, 26, render)
+      .then(callback)
+  })
 }
