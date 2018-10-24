fed9432ab736900ae9650e52504599695473c433
Wu Chengzhong <chengzhong.wu@rokid.com>
2018-10-24 21:08:59 +0800

    feat(exodus): migrate volume/rokid configs
    Change-Id: I628c02e439ed9be16918eb6d94a197bd030d6073

--- packages/@yoda/exodus/index.js
+++ packages/@yoda/exodus/index.js
@@ -1,45 +1,49 @@
 var fs = require('fs')
+var logger = require('logger')('exodus')
+var AudioManager = require('@yoda/audio').AudioManager
 var property = require('@yoda/property')
 var wifi = require('@yoda/wifi')
 var compose = require('@yoda/util').compose
-var logger = require('logger')('exodus')
 var _ = require('@yoda/util')._
 
-module.exports = exodus
-function exodus (callback) {
-  var migrated = property.get('sys.migrated.from.lua', 'persist') === '1'
-  if (migrated) {
-    logger.info('already migrated from lua, skipping.')
-    return process.nextTick(() => callback(null))
-  }
+function readConfig (path, callback) {
+  fs.stat(path, (err, stats) => {
+    if (err) {
+      if (err.code !== 'ENOENT') {
+        return callback(err)
+      }
+      logger.info(`${path} doesn't exists, skipping.`)
+      return callback(null, null)
+    }
+    if (!stats.isFile()) {
+      return callback(null, null)
+    }
+    var data
+    try {
+      data = require(path)
+    } catch (err) {
+      callback(err)
+      return
+    }
+    callback(null, data)
+  })
+}
+
+function migrationOfWiFi (callback) {
+  logger.info('migrating wifi config.')
 
   var wifiConfig = '/data/rokid_wifi.json'
 
   compose([
-    cb => fs.stat(wifiConfig, (err, stats) => {
-      if (err) {
-        if (err.code !== 'ENOENT') {
-          return cb(err)
-        }
-        logger.info(`${wifiConfig} doesn't exists, skipping.`)
-        return compose.Break()
+    cb => readConfig(wifiConfig, cb),
+    (cb, data) => {
+      if (data == null) {
+        return cb(null)
       }
-      cb(null, stats)
-    }),
-    (cb, stats) => {
-      if (!stats.isFile()) {
-        return compose.Break()
-      }
-      var data
-      try {
-        data = require(wifiConfig)
-      } catch (err) {
-        cb(err)
-        return
-      }
+
       var history = _.get(data, 'history')
       if (!Array.isArray(history)) {
-        return compose.Break()
+        return cb(null)
       }
       history.forEach(it => {
         var ssid = _.get(it, 'S')
@@ -55,14 +59,90 @@ function exodus (callback) {
         wifi.enableScanPassively()
         wifi.save()
       })
-      process.nextTick(() => cb(null))
+      cb(null)
     }
+  ], (err) => {
+    if (err) {
+      logger.error('Unexpected error on migrating WiFi config', err.stack)
+    }
+    callback()
+  })
+}
+
+function migrationOfVolume (callback) {
+  logger.info('migrating volume config.')
+
+  var volumeConfig = '/data/rokid_volume.json'
+
+  compose([
+    cb => readConfig(volumeConfig, cb),
+    (cb, data) => {
+      if (data == null) {
+        return cb(null)
+      }
+
+      var volume = _.get(data, 'volume')
+      if (typeof volume !== 'number') {
+        volume = Number(volume)
+      }
+      if (isNaN(volume)) {
+        return cb(null)
+      }
+      AudioManager.setVolume(volume)
+      cb(null)
+    }
+  ], (err) => {
+    if (err) {
+      logger.error('Unexpected error on migrating volume config', err.stack)
+    }
+    callback()
+  })
+}
+
+function migrationOfRokid (callback) {
+  logger.info('migrating rokid config.')
+
+  var rokidConfig = '/data/rokid_config.json'
+
+  compose([
+    cb => readConfig(rokidConfig, cb),
+    (cb, data) => {
+      if (data == null) {
+        return cb(null)
+      }
+
+      if (_.get(data, /** intended for history reason */'qqmusic')) {
+        property.set('sys.firstboot.init', '1', 'persist')
+      }
+      cb(null)
+    }
+  ], (err) => {
+    if (err) {
+      logger.error('Unexpected error on migrating rokid config', err.stack)
+    }
+    callback()
+  })
+}
+
+module.exports = exodus
+function exodus (callback) {
+  var migrationKey = 'sys.migration.lua'
+  var migrated = property.get(migrationKey, 'persist') === '1'
+  if (migrated) {
+    logger.info('already migrated from lua, skipping.')
+    return process.nextTick(() => callback(null))
+  }
+
+  compose([
+    migrationOfWiFi,
+    migrationOfVolume,
+    migrationOfRokid
   ], (err) => {
     if (err) {
       return callback(err)
     }
     logger.info(`Now begins the new life.`)
-    property.set('sys.migrated.from.lua', '1', 'persist')
+    property.set(migrationKey, '1', 'persist')
     callback(null)
   })
 }
