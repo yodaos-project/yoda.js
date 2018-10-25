d2974ad2773f2b11f5f0adbbc11e5d35feddb57d
Yorkie Liu <yorkiefixer@gmail.com>
2018-10-25 21:15:49 +0800

    light: dont play the startup
    Change-Id: Id0cf04352f22091157bcd3897824005eb315f735

--- res/light/setWelcome.js
+++ res/light/setWelcome.js
@@ -1,8 +1,6 @@
 'use strict'
 
 module.exports = function (light, data, callback) {
-  var rand = Math.round(Math.random() * 2)
-  var player = light.sound(`system://startup${rand}.ogg`)
   light.requestAnimationFrame(() => {
     light.transition({r: 0, g: 0, b: 0}, {r: 0, g: 0, b: 255}, 2400, 26, (r, g, b, lastFrame) => {
       light.fill(r, g, b)
