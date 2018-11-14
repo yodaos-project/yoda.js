e41dfce8a6fa84b20d05fc495afa74ac936d8a9a
morrowind <chenfeng.xie@rokid.com>
2018-11-14 15:42:14 +0800

    test(timer): Add timer apps' unit test case..
    Change-Id: Ic38d54d4a9910cf0530eecb093f352d133ae992c

---
+++ test/apps/timer/timer-test.js
@@ -0,0 +1,19 @@
+'use strict'
+
+var test = require('tape')
+var mock = require('@yoda/mock')
+
+test('Timer: test stop timer while timer is not started.', t => {
+  mock.mockAppRuntime('/opt/apps/timer')
+    .then(runtime => {
+      runtime.restore()
+      runtime.mockService('tts', 'speak', function (appid, text) {
+        console.log(text)
+        t.equal(appid, '@yoda/timer')
+        t.equal(text, '计时器未打开')
+        t.end()
+        return Promise.resolve([this.ttsId++])
+      })
+      runtime.onVoiceCommand('', {intent: 'timer_close', appId: 'RFCBA81EECAC4E11BA0EBC1AEC397A93'})
+    })
+})
