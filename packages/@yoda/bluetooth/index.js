475e3d40db83e9bf3d0d9ac6f0b0cfab13a50d96
Morrowind Xie <morrowindxie@gmail.com>
2018-12-23 15:08:59 +0800

    bluetooth: add unified a2dp adapter. (#131)
    1. transfer a2dp-sink mode
    2. support a2dp-source mode
    3. manage sink & source mode together and handle their interaction
    4. check states which send from bluetooth service in this adapter and emit more clear defined events for receiver.
    5. add getAdapter interface in module access

--- packages/@yoda/bluetooth/index.js
+++ packages/@yoda/bluetooth/index.js
@@ -11,8 +11,11 @@
  * - Transfer data to and from other devices.
  */
 
-var messageStreamInstance
-var playerInstance
+var logger = require('logger')('bluetooth-index')
+var system = require('@yoda/system')
+var messageStreamInstance = null
+var playerInstance = null
+var a2dpInstance = null
 
 module.exports = {
   /**
@@ -51,5 +54,39 @@ module.exports = {
       playerInstance.disconnect()
       playerInstance = null
     }
-  }
+    if (a2dpInstance) {
+      a2dpInstance.destroy()
+      a2dpInstance = null
+    }
+  },
+  /**
+   * @typedef {Object} PROFILE
+   * @property {string} BLE - Bluetooth low energy profile.
+   * @property {string} A2DP - Bluetooth advanced audio distribution profile.
+   */
+  /**
+   * Get bluetooth adapter by profile name.
+   * @param {PROFILE} profile - The profile name.
+   */
+  getAdapter: function (profile) {
+    logger.debug(`getAdapter(${profile})`)
+    switch (profile) {
+      case this.protocol.PROFILE.A2DP:
+        var BluetoothA2dp = require('./a2dp').BluetoothA2dp
+        a2dpInstance = new BluetoothA2dp(system.getDeviceName())
+        logger.info('Create -- BluetoothA2dp --')
+        return a2dpInstance
+      case this.protocol.PROFILE.BLE:
+      default:
+        return this.getMessageStream()
+    }
+  },
+  /**
+   * Get bluetooth protocol constant difinitions.
+   * @example
+   * var bluetooth = require('@yoda/bluetooth')
+   * var protocol = bluetooth.protocol
+   * var a2dp = bluetooth.getAdapter(protocol.PROFILE.A2DP)
+   */
+  protocol: require('./protocol.json')
 }
