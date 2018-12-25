114ec1cb91f512e85d2b66b50f9756d8e3555348
gaosp <shiping.gao@rokid.com>
2018-12-25 21:24:17 +0800

    update test

--- test/@yoda/trace/index.test.js
+++ test/@yoda/trace/index.test.js
@@ -4,11 +4,20 @@ var test = require('tape')
 var upload = require('@yoda/trace')
 
 test('array check', (t) => {
-  upload({
-    eventId: 'datacollection-test',
-    eventName: 'datacollection-test',
-    eventType: 1
-  })
+  t.throws(() => {
+    upload({
+      eventId: 'datacollection-test',
+      eventName: 'datacollection-test',
+      eventType: 1
+    })
+  }, new RegExp('Expect a object Array on traces'), 'expect traces is a object array')
+  t.end()
+})
+
+test('array length check', (t) => {
+  t.throws(() => {
+    upload([])
+  }, new RegExp('Expect traces length greater than 0'), 'expect traces length greater than 0')
   t.end()
 })
 
