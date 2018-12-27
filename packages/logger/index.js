5123ae793345b3ea916c016512e08e2a34ba4183
qile222 <chhxxc@gmail.com>
2018-12-27 17:18:11 +0800

    logger: add cloud log switch (#147)

--- packages/logger/index.js
+++ packages/logger/index.js
@@ -88,9 +88,6 @@ Logger.prototype.warn = createLoggerFunction('warn')
  */
 Logger.prototype.error = createLoggerFunction('error')
 
-// disable cloud by default
-native.enableCloud(0)
-
 /**
  * @example
  * var logger = require('logger')('some tag')
@@ -103,3 +100,43 @@ native.enableCloud(0)
 module.exports = function (name) {
   return new Logger(name)
 }
+
+var UPLOAD_DISABLE_LEVEL = 0
+var UPLOAD_MIN_LEVEL = 1
+var UPLOAD_MAX_LEVEL = 5
+
+/**
+ * set upload level to cloud
+ *
+ * @example
+ * var setGlobalUploadLevel = require('logger').setGlobalUploadLevel
+ * // enable
+ * setGlobalUploadLevel(level, "your gw token")
+ * // disable
+ * setGlobalUploadLevel(UPLOAD_DISABLE_LEVEL)
+ *
+ * @function defaults
+ * @param {number} level - set UPLOAD_DISABLE_LEVEL to disable;
+ *                         set level between
+ *                         [UPLOAD_MIN_LEVEL, UPLOAD_MAX_LEVEL] to enable
+ * @param {string} token - cloudgw token
+ * @throws {error} level out of range or missing authorization
+ */
+module.exports.setGlobalUploadLevel = function (level, authorization) {
+  if (level === UPLOAD_DISABLE_LEVEL) {
+    native.enableCloud(UPLOAD_DISABLE_LEVEL, '')
+  } else if (UPLOAD_MIN_LEVEL <= level && level <= UPLOAD_MAX_LEVEL) {
+    if (!authorization) {
+      throw new Error('missing cloudgw authorization')
+    }
+    native.enableCloud(level, authorization)
+  } else {
+    throw new Error(
+      `upload level should between ${UPLOAD_MIN_LEVEL},${UPLOAD_MAX_LEVEL}`
+    )
+  }
+}
+
+module.exports.UPLOAD_DISABLE_LEVEL = UPLOAD_DISABLE_LEVEL
+module.exports.UPLOAD_MIN_LEVEL = UPLOAD_MIN_LEVEL
+module.exports.UPLOAD_MAX_LEVEL = UPLOAD_MAX_LEVEL
