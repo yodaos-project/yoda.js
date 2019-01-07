6b45ad1b307df3bb715963994b92d58717703854
legendecas <legendecas@gmail.com>
2019-01-07 22:37:25 +0800

    flora-disposable: remove client name to prevent conflicts

--- packages/@yoda/flora/disposable.js
+++ packages/@yoda/flora/disposable.js
@@ -5,7 +5,7 @@ function once (name, options) {
   if (options == null) {
     options = {}
   }
-  var url = options.url || `unix:/var/run/flora.sock#disposable_for_${name}`
+  var url = options.url || `unix:/var/run/flora.sock`
   var timeout = options.timeout || 15000
   var agent = new flora.Agent(url)
   return new Promise((resolve, reject) => {
