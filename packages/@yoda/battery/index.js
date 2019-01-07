dbc2704714ca889f4ff485e98d74d25ae615c1db
legendecas <legendecas@gmail.com>
2019-01-07 22:26:06 +0800

    battery: parse flora results on @yoda/battery

--- packages/@yoda/battery/index.js
+++ packages/@yoda/battery/index.js
@@ -7,8 +7,23 @@
 
 var floraDisposable = require('@yoda/flora/disposable')
 
+/**
+ *
+ * @param {object} [options]
+ * @param {number} [options.timeout]
+ * @returns {object} battery info
+ */
 function getBatteryInfo (options) {
   return floraDisposable.once('battery.info', options)
+    .then(msg => {
+      var data
+      try {
+        data = JSON.parse(msg[0])
+      } catch (err) {
+        throw new Error('Unparsable data received from battery.info')
+      }
+      return data
+    })
 }
 
 module.exports.getBatteryInfo = getBatteryInfo
