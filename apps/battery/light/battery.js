239faad793f19d1854367ff8d3311554fe4630f9
PuppiesMeat <46046512+PuppiesMeat@users.noreply.github.com>
2019-01-09 18:33:18 +0800

    battery-app: fix battery cconnect media error (#241)

--- apps/battery/light/battery.js
+++ apps/battery/light/battery.js
@@ -2,8 +2,8 @@ var path = require('path')
 
 module.exports = function (light, data, callback) {
   var appMediaPath = path.join(__dirname, '..', 'media')
-  if (data.isAcConnected) {
-    light.sound('self://res/battery_connecte.ogg', appMediaPath, { ignore: false })
+  if (data.isAcConnect) {
+    light.sound('self://res/battery_connect.ogg', appMediaPath, { ignore: false })
   } else {
     light.sound('self://res/battery_disconnect.ogg', appMediaPath, { ignore: false })
   }
