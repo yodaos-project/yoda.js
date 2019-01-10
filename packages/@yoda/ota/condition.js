b54b7f234a78fcba9c4851c343764615b0c6a624
legendecas <legendecas@gmail.com>
2019-01-10 21:18:03 +0800

    ota: conditions on force update check

---
+++ packages/@yoda/ota/condition.js
@@ -0,0 +1,24 @@
+var promisify = require('util').promisify
+var property = require('property')
+
+var fetchOtaInfo = require('./network').fetchOtaInfo
+
+var fetchOtaInfoAsync = promisify(fetchOtaInfo)
+
+module.exports.getAvailabilityOfOta = getAvailabilityOfOta
+function getAvailabilityOfOta (upgradeInfo) {
+  var localVersion = property.get('ro.build.version.release')
+  // TODO: battery assertion
+  return fetchOtaInfoAsync(localVersion)
+    .then(
+      info => {
+        if (info == null) {
+          return false
+        }
+        if (info.version !== upgradeInfo.version) {
+          return false
+        }
+        return true
+      }
+    )
+}
