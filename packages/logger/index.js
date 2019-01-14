a16e4219acaea8854d073e0775ae055f9c7d951b
qile222 <chhxxc@gmail.com>
2019-01-14 18:56:06 +0800

    logger: add log level switch timeout (#207)

--- packages/logger/index.js
+++ packages/logger/index.js
@@ -22,6 +22,7 @@ if (process.platform !== 'darwin') {
 }
 
 var logLevels = {
+  'none': 0,
   'verbose': 1,
   'debug': 2,
   'info': 3,
@@ -101,42 +102,36 @@ module.exports = function (name) {
   return new Logger(name)
 }
 
-var UPLOAD_DISABLE_LEVEL = 0
-var UPLOAD_MIN_LEVEL = 1
-var UPLOAD_MAX_LEVEL = 5
-
 /**
  * set upload level to cloud
  *
  * @example
  * var setGlobalUploadLevel = require('logger').setGlobalUploadLevel
  * // enable
- * setGlobalUploadLevel(level, "your gw token")
+ * setGlobalUploadLevel(level, "your gw authorization")
  * // disable
- * setGlobalUploadLevel(UPLOAD_DISABLE_LEVEL)
+ * setGlobalUploadLevel(logLevels.none)
  *
  * @function defaults
- * @param {number} level - set UPLOAD_DISABLE_LEVEL to disable;
+ * @param {number} level - set logLevels.none to disable;
  *                         set level between
- *                         [UPLOAD_MIN_LEVEL, UPLOAD_MAX_LEVEL] to enable
- * @param {string} token - cloudgw token
+ *                         [verbose, error] to enable
+ * @param {string} authorization - cloudgw authorization
  * @throws {error} level out of range or missing authorization
  */
 module.exports.setGlobalUploadLevel = function (level, authorization) {
-  if (level === UPLOAD_DISABLE_LEVEL) {
-    native.enableCloud(UPLOAD_DISABLE_LEVEL, '')
-  } else if (UPLOAD_MIN_LEVEL <= level && level <= UPLOAD_MAX_LEVEL) {
+  if (level === logLevels.none) {
+    native.enableCloud(logLevels.none, '')
+  } else if (logLevels.verbose <= level && level <= logLevels.error) {
     if (!authorization) {
       throw new Error('missing cloudgw authorization')
     }
     native.enableCloud(level, authorization)
   } else {
     throw new Error(
-      `upload level should between ${UPLOAD_MIN_LEVEL},${UPLOAD_MAX_LEVEL}`
+      `upload level should between [${logLevels.verbose},${logLevels.error}]`
     )
   }
 }
 
-module.exports.UPLOAD_DISABLE_LEVEL = UPLOAD_DISABLE_LEVEL
-module.exports.UPLOAD_MIN_LEVEL = UPLOAD_MIN_LEVEL
-module.exports.UPLOAD_MAX_LEVEL = UPLOAD_MAX_LEVEL
+module.exports.levels = logLevels
