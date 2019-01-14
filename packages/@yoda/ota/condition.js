c6da5ee8c6d96aeb2c8db42acde879659fb25f6f
legendecas <legendecas@gmail.com>
2019-01-14 10:28:04 +0800

    ota: ota conditions on battery states (#285)

--- packages/@yoda/ota/condition.js
+++ packages/@yoda/ota/condition.js
@@ -1,5 +1,7 @@
 var promisify = require('util').promisify
 var property = require('@yoda/property')
+var manifest = require('@yoda/manifest')
+var battery = require('@yoda/battery')
 var fetchOtaInfo = require('./network').fetchOtaInfo
 
 var fetchOtaInfoAsync = promisify(fetchOtaInfo)
@@ -7,10 +9,24 @@ var fetchOtaInfoAsync = promisify(fetchOtaInfo)
 module.exports.getAvailabilityOfOta = getAvailabilityOfOta
 function getAvailabilityOfOta (upgradeInfo) {
   var localVersion = property.get('ro.build.version.release')
-  // TODO: battery assertion
-  return fetchOtaInfoAsync(localVersion)
-    .then(
-      info => {
+  var future = Promise.resolve(true)
+  if (manifest.isCapabilityEnabled('battery')) {
+    future = battery.getBatteryInfo()
+      .then(info => {
+        if (info.batSupported) {
+          if (!info.batChargingOnline && info.batLevel < 50) {
+            return false
+          }
+        }
+        return true
+      })
+  }
+  return future.then(check => {
+    if (!check) {
+      return false
+    }
+    return fetchOtaInfoAsync(localVersion)
+      .then(info => {
         if (info == null) {
           return false
         }
@@ -18,6 +34,6 @@ function getAvailabilityOfOta (upgradeInfo) {
           return false
         }
         return true
-      }
-    )
+      })
+  })
 }
---
+++ test/@yoda/ota/condition.test.js
@@ -0,0 +1,85 @@
+var test = require('tape')
+var util = require('util')
+var mock = require('../../helper/mock')
+var otaNetwork = require('@yoda/ota/network')
+var mockedFetchInfo = () => Promise.reject(new Error('mock required'))
+otaNetwork.fetchOtaInfo[util.promisify.custom] = function noop (version) {
+  return mockedFetchInfo(version)
+}
+
+var condition = require('@yoda/ota/condition')
+var battery = require('@yoda/battery')
+var manifest = require('@yoda/manifest')
+
+test('ota shall be available if fetched info is same with pending update', t => {
+  t.plan(1)
+  mockedFetchInfo = () => Promise.resolve({ version: '1' })
+  mock.mockReturns(manifest, 'isCapabilityEnabled', false)
+  condition.getAvailabilityOfOta({ version: '1' })
+    .then(available => {
+      t.strictEqual(available, true)
+    })
+})
+
+test('ota shall be available if battery is charging', t => {
+  t.plan(1)
+  mockedFetchInfo = () => Promise.resolve({ version: '1' })
+  mock.mockReturns(manifest, 'isCapabilityEnabled', true)
+  mock.mockPromise(battery, 'getBatteryInfo', null, {
+    batSupported: true,
+    batChargingOnline: true,
+    batLevel: 49
+  })
+  condition.getAvailabilityOfOta({ version: '1' })
+    .then(available => {
+      t.strictEqual(available, true)
+    })
+})
+
+test('ota shall be available if battery is not charging yet power level is sufficient', t => {
+  t.plan(1)
+  mockedFetchInfo = () => Promise.resolve({ version: '1' })
+  mock.mockReturns(manifest, 'isCapabilityEnabled', true)
+  mock.mockPromise(battery, 'getBatteryInfo', null, {
+    batSupported: true,
+    batChargingOnline: false,
+    batLevel: 51
+  })
+  condition.getAvailabilityOfOta({ version: '1' })
+    .then(available => {
+      t.strictEqual(available, true)
+    })
+})
+
+test('ota shall not be available if no update info could be fetched', t => {
+  t.plan(1)
+  mockedFetchInfo = () => Promise.resolve({ version: '1' })
+  condition.getAvailabilityOfOta({})
+    .then(available => {
+      t.strictEqual(available, false)
+    })
+})
+
+test('ota shall not be available if fetched update info is not equal to pending update', t => {
+  t.plan(1)
+  mockedFetchInfo = () => Promise.resolve({ version: '2' })
+  condition.getAvailabilityOfOta({ version: '1' })
+    .then(available => {
+      t.strictEqual(available, false)
+    })
+})
+
+test('ota shall not be available if battery is low power level and not charging', t => {
+  t.plan(1)
+  mockedFetchInfo = () => Promise.resolve({ version: '1' })
+  mock.mockReturns(manifest, 'isCapabilityEnabled', true)
+  mock.mockPromise(battery, 'getBatteryInfo', null, {
+    batSupported: true,
+    batChargingOnline: false,
+    batLevel: 49
+  })
+  condition.getAvailabilityOfOta({ version: '1' })
+    .then(available => {
+      t.strictEqual(available, false)
+    })
+})
