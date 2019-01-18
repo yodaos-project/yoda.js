9daf018f272825f14b1dd3055897dc4ac0d782ee
13588478764 <13588478764@163.com>
2019-01-18 21:56:10 +0800

    del callback ,use promise

--- apps/playercontrol/app.js
+++ apps/playercontrol/app.js
@@ -7,6 +7,7 @@ var DIRPATH = '/data/AppData/playercontrol'
 var fsStat = util.promisify(fs.stat)
 var readFile = util.promisify(fs.readFile)
 var writeFile = util.promisify(fs.writeFile)
+var mkdir = util.promisify(fs.mkdir)
 module.exports = function (activity) {
   var STRING_NO_PLAYER_EXIST = '当前没在播放状态呢，如果你想听歌，请对我说，播放歌曲'
 
@@ -19,77 +20,67 @@ module.exports = function (activity) {
   activity.on('request', function (nlp, action) {
     logger.log('player control event: request', nlp)
     if (nlp.intent === 'ROKID.INTENT.RESUME') {
-      readconfig(function (playerInfo, error) {
-        logger.log('have error ', error)
-        if (playerInfo === null || playerInfo === undefined || playerInfo === '') {
-          speakAndExit(STRING_NO_PLAYER_EXIST)
-          return
-        }
-        logger.log('name = ', playerInfo.name)
-        if (playerInfo.name === null || playerInfo.name === undefined || playerInfo.name === '') {
-          speakAndExit(STRING_NO_PLAYER_EXIST)
-        } else if (playerInfo.name === '@yoda/bluetooth') {
-          activity.openUrl('yoda-skill://bluetooth_music/bluetooth_start_bluetooth_music', { preemptive: true })
-        } else {
-          logger.log('url = ', playerInfo.url)
-          if (playerInfo.url === null || playerInfo.url === undefined || playerInfo.url === '') {
+      readconfig()
+        .then(playerInfo => {
+          logger.log('playerInfo ', playerInfo)
+          if (playerInfo === null || playerInfo === undefined || playerInfo === '') {
             speakAndExit(STRING_NO_PLAYER_EXIST)
             return
           }
-          activity.openUrl(playerInfo.url, { preemptive: true })
-        }
-      })
+          logger.log('name = ', playerInfo.name)
+          if (playerInfo.name === null || playerInfo.name === undefined || playerInfo.name === '') {
+            speakAndExit(STRING_NO_PLAYER_EXIST)
+          } else if (playerInfo.name === '@yoda/bluetooth') {
+            activity.openUrl('yoda-skill://bluetooth_music/bluetooth_start_bluetooth_music', { preemptive: true })
+          } else {
+            logger.log('url = ', playerInfo.url)
+            if (playerInfo.url === null || playerInfo.url === undefined || playerInfo.url === '') {
+              speakAndExit(STRING_NO_PLAYER_EXIST)
+              return
+            }
+            activity.openUrl(playerInfo.url, { preemptive: true })
+          }
+        })
     }
   })
 
-  function readconfig (callback) {
-    fsStat(DATAPATH).then(() => {
-      readFile(DATAPATH, 'utf8').then((data) => {
-        logger.log('data = ', data)
-        callback(JSON.parse(data || '{}'), null)
+  function readconfig () {
+    return fsStat(DATAPATH)
+      .then(() => {
+        return readFile(DATAPATH, 'utf8')
       })
-        .catch((error) => {
-          logger.error('read data error', error.stack)
-          callback(null, error)
-        })
-    })
-      .catch((err) => {
-        logger.error('read dir error', err)
-        callback(null, err)
+      .then((data) => {
+        logger.log('data = ', data)
+        return JSON.parse(data || '{}')
+      })
+      .catch(err => {
+        logger.error('unexpected error on read config', err.stack)
+        return {}
       })
   }
 
   function saveconfig (data) {
-    fsStat(DIRPATH).then(() => {
-      writeFile(DATAPATH, JSON.stringify(data)).then(() => {
-        logger.log('writeFile file success', data)
+    return fsStat(DIRPATH)
+      .then(() => {
+        return writeFile(DATAPATH, JSON.stringify(data))
       })
-        .catch((excp) => {
-          logger.error('playercontrol set config: update local data error', excp && excp.stack)
-          return false
-        })
-    })
+      .then(() => {
+        return {}
+      })
       .catch((err) => {
         if (err.code === 'ENOENT') {
-          var mkdir = util.promisify(fs.mkdir)
-          mkdir(DIRPATH).then(() => {
-            logger.log('create dir success')
-            writeFile(DATAPATH, JSON.stringify(data)).then(() => {
-              logger.log('w', data)
-              return true
+          return mkdir(DIRPATH)
+            .then(() => {
+              logger.log('create dir success')
+              return writeFile(DATAPATH, JSON.stringify(data))
             })
-              .catch((excp) => {
-                logger.error('playercontrol set config: update local data error', excp && excp.stack)
-                return false
-              })
-          })
             .catch((error) => {
-              logger.error(error)
-              return false
+              logger.error('unexpected error on saveconfig', error.stack)
+              return {}
             })
         } else {
-          logger.error(err)
-          return false
+          logger.error('unexpected error on saveconfig ex', err)
+          return {}
         }
       })
   }
