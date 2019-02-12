689f3bd328e542f0146436291583489decc12309
Yazhong Liu <yorkiefixer@gmail.com>
2019-02-12 17:39:22 +0800

    Update runtime/lib/component/turen.js
    Co-Authored-By: legendecas <legendecas@gmail.com>

--- runtime/lib/component/turen.js
+++ runtime/lib/component/turen.js
@@ -504,7 +504,7 @@ Turen.prototype.handleSpeechError = function handleSpeechError (errCode) {
      * Currently @yoda/system handles speech error in a such quick way, yet for
      * some reason apps like cloud-app-client could not determine tts/media
      * status in such a short time(events have to be transferred through
-     * 2/3 ipc).
+     * 2-3 ipc).
      * Thus just closing cut app here works as expected, and shall be fixed
      * with a invocation queue in translator-ipc.
      */
