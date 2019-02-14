Bhanditz/yodart

add delegate runtimeDidWakeup add reload request for custom-config add test
Change-Id: Ic8fc98d95788af7089ecdc1f06aba3753c3b2ac1

runtime/lib/app-runtime.js
@@ -389,6 +389,7 @@ AppRuntime.prototype.wakeup = function wakeup (options) {
 
   this.component.custodian.resetState()
   this.component.custodian.prepareNetwork()
+  this.component.dispatcher.delegate('runtimeDidWakeup')
 }
 
 /**

test/component/custom-config/custom-config.test.js
@@ -1,0 +1,55 @@
+'use strict'
+
+var test = require('tape')
+var helper = require('../../helper')
+var CustomConfig = require(`${helper.paths.runtime}/lib/component/custom-config`)
+
+test('custom-config check', function (t) {
+  var runtime = {}
+  var liftTimeCb
+  runtime.setPickup = function () {
+  }
+  runtime.component = {
+    lifetime: {
+      on: function (event, cb) {
+        t.strictEqual(event, 'eviction')
+        liftTimeCb = cb
+      }
+    },
+    dndMode: {
+      setOption: function (option) {
+        t.strictEqual(option.startTime, '23:00')
+      }
+    }
+  }
+  var firstConfig = {
+    standbyLight: '{"action":"open"}',
+    continuousDialog: '{"action":"close"}'
+  }
+  var config = {
+    nightMode: {
+      startTime: '23:00',
+      endTime: '18:00',
+      action: 'close'
+    },
+    standbyLight: {
+      action: 'close'
+    }
+  }
+  var customConfig = new CustomConfig(runtime)
+  runtime.openUrl = function (url, option) {
+    t.strictEqual(url, 'yoda-skill://custom-config/firstLoad?config={"standbyLight":"{\\"action\\":\\"open\\"}","continuousDialog":"{\\"action\\":\\"close\\"}"}')
+  }
+  customConfig.onLoadCustomConfig(JSON.stringify(firstConfig))
+  runtime.openUrl = function (url, option) {
+    t.strictEqual(url, 'yoda-skill://custom-config/reload')
+  }
+  customConfig.runtimeDidWakeup()
+  runtime.openUrl = function (url, option) {
+    t.strictEqual(url, 'yoda-skill://custom-config/standbyLight?action=close')
+  }
+  liftTimeCb('123', 'cut')
+  liftTimeCb('123', 'scene')
+  customConfig.onCustomConfig(JSON.stringify(config))
+  t.end()
+})

yodaos-project/yoda.js

runtime/lib/app-runtime.js
@@ -389,6 +389,7 @@ AppRuntime.prototype.wakeup = function wakeup (options) {
 
   this.component.custodian.resetState()
   this.component.custodian.prepareNetwork()
+  this.component.dispatcher.delegate('runtimeDidResumeFromSleep')
 }
 
 /**

test/component/custom-config/custom-config.test.js
@@ -1,0 +1,55 @@
+'use strict'
+
+var test = require('tape')
+var helper = require('../../helper')
+var CustomConfig = require(`${helper.paths.runtime}/lib/component/custom-config`)
+
+test('custom-config check', function (t) {
+  var runtime = {}
+  var liftTimeCb
+  runtime.setPickup = function () {
+  }
+  runtime.component = {
+    lifetime: {
+      on: function (event, cb) {
+        t.strictEqual(event, 'eviction')
+        liftTimeCb = cb
+      }
+    },
+    dndMode: {
+      setOption: function (option) {
+        t.strictEqual(option.startTime, '23:00')
+      }
+    }
+  }
+  var firstConfig = {
+    standbyLight: '{"action":"open"}',
+    continuousDialog: '{"action":"close"}'
+  }
+  var config = {
+    nightMode: {
+      startTime: '23:00',
+      endTime: '18:00',
+      action: 'close'
+    },
+    standbyLight: {
+      action: 'close'
+    }
+  }
+  var customConfig = new CustomConfig(runtime)
+  runtime.openUrl = function (url, option) {
+    t.strictEqual(url, 'yoda-skill://custom-config/firstLoad?config={"standbyLight":"{\\"action\\":\\"open\\"}","continuousDialog":"{\\"action\\":\\"close\\"}"}')
+  }
+  customConfig.onLoadCustomConfig(JSON.stringify(firstConfig))
+  runtime.openUrl = function (url, option) {
+    t.strictEqual(url, 'yoda-skill://custom-config/reload')
+  }
+  customConfig.runtimeDidResumeFromSleep()
+  runtime.openUrl = function (url, option) {
+    t.strictEqual(url, 'yoda-skill://custom-config/standbyLight?action=close')
+  }
+  liftTimeCb('123', 'cut')
+  liftTimeCb('123', 'scene')
+  customConfig.onCustomConfig(JSON.stringify(config))
+  t.end()
+})