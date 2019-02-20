87df89f0b64fa9637c4ff31f9dc9cd75faddaa46
LanFly <1260022720@qq.com>
2019-02-20 21:09:52 +0800

    add tests

--- test/apps/cloudAppClient/directive-sort.test.js
+++ test/apps/cloudAppClient/directive-sort.test.js
@@ -12,13 +12,49 @@ test('test directive sort', function (t) {
     }
   })
   var dts = [{
-    type: 'tts',
+    type: 'voice',
     action: 'pause'
   }, {
     type: 'native'
+  }, {
+    type: 'pickup'
+  }, {
+    type: 'media',
+    action: 'stop'
   }]
 
   skill.transform(dts)
 
-  t.strictEqual(skill.directives[0].type, 'native', 'The directive type native should always be front of tts')
+  assert(t, skill.directives.map((dt) => dt.type), ['native', 'tts', 'media', 'pickup'], 'The directve order should always be: native, tts, media, pickup')
 })
+
+test('test directive sort unknow', function (t) {
+  t.plan(1)
+  var skill = new Skill({}, {}, {
+    appId: 'testAppId',
+    response: {
+      action: {}
+    }
+  })
+  var dts = [{
+    type: 'voice',
+    action: 'pause'
+  }, {
+    type: 'native'
+  }, {
+    type: 'unknow'
+  }, {
+    type: 'pickup'
+  }, {
+    type: 'media',
+    action: 'stop'
+  }]
+
+  skill.transform(dts)
+
+  assert(t, skill.directives.map((dt) => dt.type), ['native', 'tts', 'media', 'pickup'], 'The unknown directve should be ignore and remove')
+})
+
+function assert (t, actual, expected, msg) {
+  t.strictEqual(actual.join(''), expected.join(''), msg)
+}
