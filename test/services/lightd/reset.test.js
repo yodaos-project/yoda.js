8ba8bfe42dfa7a04e1276c90f1189281d2eb840e
LanFly <1260022720@qq.com>
2019-02-21 16:32:17 +0800

    fixed test

--- test/services/lightd/reset.test.js
+++ test/services/lightd/reset.test.js
@@ -5,22 +5,20 @@ var effect = new Effect()
 var light = new Service(effect)
 
 test('reset should be ok', t => {
-  setTimeout(() => {
-    light.reset()
-    var systemEmpty = true
-    light.systemspaceZIndex.forEach(element => {
-      if (element !== undefined) {
-        systemEmpty = false
-      }
-    })
-    var userEmpty = true
-    light.userspaceZIndex.forEach(element => {
-      if (element !== undefined) {
-        userEmpty = false
-      }
-    })
-    t.strictEqual(systemEmpty, true, 'systemspaceZIndex should be empty!')
-    t.strictEqual(userEmpty, true, 'userEmpty should be empty!')
-    t.end()
-  }, 5000)
+  light.reset()
+  var systemEmpty = true
+  light.systemspaceZIndex.forEach(element => {
+    if (element !== undefined) {
+      systemEmpty = false
+    }
+  })
+  var userEmpty = true
+  light.userspaceZIndex.forEach(element => {
+    if (element !== undefined) {
+      userEmpty = false
+    }
+  })
+  t.strictEqual(systemEmpty, true, 'systemspaceZIndex should be empty!')
+  t.strictEqual(userEmpty, true, 'userEmpty should be empty!')
+  t.end()
 })
