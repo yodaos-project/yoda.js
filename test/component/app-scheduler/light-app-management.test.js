9ed0e58194826e2686855bcdffb7feb8d1deb0fc
legendecas <legendecas@gmail.com>
2019-03-01 17:44:54 +0800

    light-app: app should be exited if crashes on startup

--- test/component/app-scheduler/light-app-management.test.js
+++ test/component/app-scheduler/light-app-management.test.js
@@ -2,25 +2,27 @@ var test = require('tape')
 var path = require('path')
 
 var helper = require('../../helper')
-var mock = require('./mock')
+var mock = require('../../helper/mock')
+var getLoader = require('./mock').getLoader
 var Scheduler = require(`${helper.paths.runtime}/lib/component/app-scheduler`)
 
 require('@yoda/oh-my-little-pony')
 
-test('shall create child process', t => {
+test('shall create light app', t => {
   var target = path.join(helper.paths.fixture, 'noop-app')
-  t.plan(5)
+  t.plan(4)
   var appId = '@test'
   var runtime = {
     appGC: function () {},
     component: {
-      appLoader: mock.getLoader({
+      appLoader: getLoader({
         '@test': {
           appHome: target
         }
       })
     }
   }
+  mock.mockReturns(runtime.component.appLoader, 'getTypeOfApp', 'light')
   var scheduler = new Scheduler(runtime)
   t.looseEqual(scheduler.appStatus[appId], null)
   var promise = scheduler.createApp(appId)
@@ -31,11 +33,6 @@ test('shall create child process', t => {
     .then(app => {
       t.notLooseEqual(scheduler.appMap[appId], null)
       t.strictEqual(scheduler.appStatus[appId], 'running')
-
-      app.once('exit', () => {
-        t.looseEqual(scheduler.appStatus[appId], 'exited')
-      })
-      scheduler.suspendApp(appId)
     })
     .catch(err => {
       t.error(err)
@@ -43,20 +40,21 @@ test('shall create child process', t => {
     })
 })
 
-test('app exits on start up', t => {
+test('light app exits on start up', t => {
   var target = path.join(helper.paths.fixture, 'crash-on-startup-app')
   t.plan(5)
   var appId = '@test'
   var runtime = {
     appGC: function () {},
     component: {
-      appLoader: mock.getLoader({
+      appLoader: getLoader({
         '@test': {
           appHome: target
         }
       })
     }
   }
+  mock.mockReturns(runtime.component.appLoader, 'getTypeOfApp', 'light')
   var scheduler = new Scheduler(runtime)
   t.looseEqual(scheduler.appStatus[appId], null)
   var promise = scheduler.createApp(appId)
