faabff5dba48cf72bb44df30c4e8154841b40a91
sudo <1260022720@qq.com>
2019-03-04 15:22:49 +0800

    fixed(lightd): fix the error in priority judgment (#536)

--- test/services/lightd/loadfile.test.js
+++ test/services/lightd/loadfile.test.js
@@ -1,21 +1,32 @@
 var Service = require('/usr/yoda/services/lightd/service')
 var test = require('tape')
-var Effect = require('/usr/yoda/services/lightd/effects')
-var effect = new Effect()
-var light = new Service(effect)
 
 test('loadfile should be ok if uri is exited--setStandby.js', t => {
-  var rst = light.loadfile('@yoda', '/opt/light/loading.js', {}, {'shouldResume': true}, (err) => {
+  var service = new Service()
+  var rst = service.loadfile('@yoda', '/opt/light/loading.js', {}, {'shouldResume': true}, (err) => {
     t.ok(err === undefined)
   })
   t.strictEqual(rst, true, 'play setStandby')
-  rst = light.loadfile('@yoda', '/opt/light/setSpeaking.js', {}, {}, (err) => {
+  rst = service.loadfile('@yoda', '/opt/light/setSpeaking.js', {}, {}, (err) => {
     t.ok(err === undefined)
   })
   t.strictEqual(rst, false, `play setSpeaking ${rst}`)
   setTimeout(() => {
-    light.stopFile('@yoda', '/opt/light/loading.js')
-    light.stopFile('@yoda', '/opt/light/setSpeaking.js')
+    service.stopFile('@yoda', '/opt/light/loading.js')
+    service.stopFile('@yoda', '/opt/light/setSpeaking.js')
     t.end()
   }, 1000)
+})
+
+test('Test priority', (t) => {
+  t.plan(2)
+  var service = new Service()
+
+  var res = service.loadfile('@testAppId', '/opt/light/loading.js', { muted: true }, { shouldResume: true }, function noop () {})
+  t.strictEqual(res, true, '/opt/light/loading.js should be render now')
+
+  res = service.loadfile('@bluetooth', '/opt/light/inCall.js', {}, { shouldResume: true, zIndex: 2 }, function noop () {})
+  t.strictEqual(res, false, '/opt/light/inCall.js should not be render.')
+
+  service.reset()
 })
