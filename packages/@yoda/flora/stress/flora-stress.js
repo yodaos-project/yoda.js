e2a8332799f3eef0df840bd72a6ea95cf11e7ae7
张晨 <ming@zhangchendeMacBook-Pro.local>
2019-03-22 21:24:29 +0800

    update flora js stress test

--- packages/@yoda/flora/stress/flora-stress.js
+++ packages/@yoda/flora/stress/flora-stress.js
@@ -2,57 +2,157 @@
 
 var uri = 'unix:/var/run/flora.sock'
 var flora = require('@yoda/flora')
-var sender
-var recv
-var sendMsg = 'flora-stress.msg.counter'
-var recvCounter = 0
-var MSG_COUNT = 1000
+var msgInfos = [
+  { name: 'msg0', arg: [ 'hello' ] },
+  { name: 'msg1', arg: [ 1, 'world' ] },
+  { name: 'msg2', arg: [ [ 'submsg' ], 1, 'world' ] },
+  { name: 'msg3', arg: undefined },
+  { name: 'msg4', arg: [ [ [ 'subsub' ] ], 1, 2, [ 'sub', 'msg' ] ] },
+  { name: 'msg5', arg: [ 100 ] },
+  { name: 'msg6', arg: [ 100, 200 ] },
+  { name: 'msg7', arg: [ 100, 200, 300 ] },
+  { name: 'msg8', arg: [ 100, '201', 300 ] },
+  { name: 'msg9', arg: [ 100, [ '201' ], 300 ] }
+]
+var methodInfos = [
+  { name: 'func0', arg: [ 'hello' ], retCode: 0 },
+  { name: 'func1', arg: [ 1, 'world' ], retCode: 100 },
+  { name: 'func2', arg: [ [ 'submsg' ], 1, 'world' ], retCode: 0 },
+  { name: 'func3', arg: undefined, retCode: -1 },
+  { name: 'func4', arg: [ [ [ 'subsub' ] ], 1, 2, [ 'sub', 'msg' ] ], retCode: -1000 },
+  { name: 'func5', arg: [ 100 ], retCode: 0 },
+  { name: 'func6', arg: [ 100, 200 ], retCode: 0 },
+  { name: 'func7', arg: [ 100, 200, 300 ], retCode: 7 },
+  { name: 'func8', arg: [ 100, '201', 300 ], retCode: 32768 },
+  { name: 'func9', arg: [ 100, [ '201' ], 300 ], retCode: 0 }
+]
+var recvNames = [
+  'stress-recv0',
+  'stress-recv1',
+  'stress-recv2',
+  'stress-recv3',
+  'stress-recv4',
+  'stress-recv5',
+  'stress-recv6',
+  'stress-recv7',
+  'stress-recv8'
+]
+var senderCount = 0
 
-function closeAgents () {
-  sender.close()
-  recv.close()
+function Receiver (name) {
+  this.name = name
 }
 
-function postMsg () {
+Receiver.prototype.start = function () {
   var i
-  for (i = 0; i < MSG_COUNT; ++i) {
-    sender.post(sendMsg + i, [ i, 'hello world', [ 10, 9, 8, 7, '6', '5', '4' ], 'byebye' ])
+  var m
+  this.agent = new flora.Agent(uri + '#' + this.name)
+  for (i = 0; i < msgInfos.length; ++i) {
+    m = msgInfos[i]
+    this.agent.subscribe(m.name, this.recvMsg)
   }
-}
-
-function restartTest () {
-  closeAgents()
-  recvCounter = 0
-  createAgents()
-  setTimeout(() => {
-    postMsg()
-  }, 100)
-}
-
-function createAgents () {
-  sender = new flora.Agent(uri + '#stress-send')
-  recv = new flora.Agent(uri + '#stress-recv')
-  sender.start()
-
-  var i
-  for (i = 0; i < MSG_COUNT; ++i) {
-    recv.subscribe(sendMsg + i, (msg, type) => {
-      console.log('received stress msg', msg[0])
-      if (msg[0] === recvCounter) {
-        ++recvCounter
-        if (recvCounter === MSG_COUNT) {
-          restartTest()
-        }
-      } else {
-        console.log('failed: received msg', msg[0], 'but excepted', recvCounter)
-        closeAgents()
-      }
+  for (i = 0; i < methodInfos.length; ++i) {
+    m = methodInfos[i]
+    this.agent.declareMethod(m.name, (msg, reply) => {
+      this.recvInvocation(msg, reply, m.retCode)
     })
   }
-  recv.start()
+  this.agent.start()
+  this.stopInFuture()
 }
 
-createAgents()
-setTimeout(() => {
-  postMsg()
-}, 100)
+Receiver.prototype.recvMsg = function (msg, type) {
+}
+
+Receiver.prototype.recvInvocation = function (msg, reply, code) {
+  reply.writeCode(code)
+  reply.writeData(msg)
+  reply.end()
+}
+
+Receiver.prototype.stopInFuture = function () {
+  var t = Math.ceil(Math.random() * 3600) + 60
+  setTimeout(() => {
+    this.agent.close()
+    this.startInFuture()
+  }, t * 1000)
+  console.log('Receiver', this.name, 'stop after', t, 'seconds')
+}
+
+Receiver.prototype.startInFuture = function () {
+  var t = Math.ceil(Math.random() * 600) + 60
+  setTimeout(() => {
+    this.start()
+  }, t * 1000)
+  console.log('Receiver', this.name, 'start after', t, 'seconds')
+}
+
+function Sender (type, cb) {
+  this.interval = Math.ceil(Math.random() * 10000) + 100
+  if (type === 0) {
+    this.send = () => {
+      if (this.index >= msgInfos.length) {
+        return false
+      }
+      var info = msgInfos[this.index]
+      this.agent.post(info.name, info.arg)
+      return true
+    }
+  } else {
+    this.send = () => {
+      if (this.index >= methodInfos.length) {
+        return false
+      }
+      var info = methodInfos[this.index]
+      var targetIndex = Math.ceil(Math.random() * 77669980) % recvNames.length
+      this.agent.call(info.name, info.arg, recvNames[targetIndex]).then(() => {}, () => {})
+      return true
+    }
+  }
+  this.index = 0
+  this.closecb = cb
+}
+
+Sender.prototype.start = function () {
+  this.agent = new flora.Agent(uri)
+  this.agent.start()
+  this.index = 0
+  this.timer = setInterval(() => {
+    if (!this.send()) {
+      clearInterval(this.timer)
+      this.agent.close()
+      this.agent = undefined
+      --senderCount
+      console.log('sender quit: count', senderCount)
+      this.closecb()
+    }
+    ++this.index
+  }, this.interval)
+  ++senderCount
+  console.log('active sender: interval', this.interval, 'count', senderCount)
+}
+
+function senderClosed (type) {
+  setTimeout(() => {
+    var s = new Sender(type, () => { senderClosed(type) })
+    s.start()
+  }, 2000)
+}
+
+var receivers = []
+var maxSender = 100
+function main () {
+  var i
+  for (i = 0; i < recvNames.length; ++i) {
+    receivers[i] = new Receiver(recvNames[i])
+    receivers[i].start()
+  }
+  for (i = 0; i < maxSender; ++i) {
+    var s = new Sender(0, () => { senderClosed(0) })
+    s.start()
+    s = new Sender(1, () => { senderClosed(1) })
+    s.start()
+  }
+}
+
+main()
