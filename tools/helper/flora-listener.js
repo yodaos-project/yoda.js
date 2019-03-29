0601df2dc704908f91f3d3a64ddacb0ad40f348e
legendecas <legendecas@gmail.com>
2019-03-29 16:09:22 +0800

    flora: support node.js (#596)

--- tools/helper/flora-listener.js
+++ tools/helper/flora-listener.js
@@ -1,14 +1,32 @@
 var flora = require('@yoda/flora')
 
-var agent = new flora.Agent('unix:/var/run/flora.sock')
+var uri = 'unix:/var/run/flora.sock'
+var names = []
+
+var cliArgs = process.argv.slice(2)
+while (cliArgs.length > 0) {
+  var $1 = cliArgs.shift()
+  switch ($1) {
+    case '-u':
+    case '--uri':
+      uri = cliArgs.shift()
+      break
+    default:
+      if ($1) {
+        names.push($1)
+      }
+  }
+}
+
+var agent = new flora.Agent(uri)
 
 var typeNames = [ 'INSTANT', 'PERSIST' ]
 
 function listen (name, msg, type) {
   var typeName = typeNames[type] || 'UNKNOWN_TYPE'
-  console.log(`${typeName}[${name}] ` + msg.map(it => JSON.stringify(it)).join(', '))
+  console.log(`[${new Date().toISOString()}] ${typeName}[${name}] ` + msg.map(it => JSON.stringify(it)).join(', '))
 }
-process.argv.slice(2).forEach(key => {
+names.forEach(key => {
   console.log(`Subscribing '${key}'...`)
   agent.subscribe(key, listen.bind(this, key))
 })
