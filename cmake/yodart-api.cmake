f6e4d644cc0d2ba4a039d2fdf8f8f0b7a62b1280
legendecas <legendecas@gmail.com>
2019-04-25 17:51:45 +0800

    build: support define node executable path

--- cmake/yodart-api.cmake
+++ cmake/yodart-api.cmake
@@ -1,8 +1,10 @@
 cmake_minimum_required(VERSION 2.8)
 
+set(HOST_NODEJS_BIN node CACHE STRING "nodejs executable path")
+
 set(YODA_API_OUTPUT "./runtime/client/api/default.json")
 file(GLOB YODA_DESCRIPTOR_FILES ./runtime/descriptor/*.js)
-add_custom_target(yodart-api ALL node tools/generate-api-json.js
+add_custom_target(yodart-api ALL ${HOST_NODEJS_BIN} tools/generate-api-json.js
   SOURCES ${YODA_DESCRIPTOR_FILES}
 )
 
