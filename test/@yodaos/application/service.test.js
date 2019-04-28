4dfca53a0653c9dd23625108067391debade2a0c
legendecas <legendecas@gmail.com>
2019-04-28 16:23:12 +0800

    yodaos-application: get service

--- test/@yodaos/application/service.test.js
+++ test/@yodaos/application/service.test.js
@@ -29,3 +29,22 @@ test('start/finish service', t => {
 
   application.startService('foo')
 })
+
+test('get service', t => {
+  t.plan(1)
+
+  var api = new EventEmitter()
+  api.appHome = path.join(__dirname, '../../fixture/noop-app')
+  api.exit = function () {
+    t.pass('should exit app process on no task available')
+  }
+
+  var application = yodaos.Application({}, api)
+  var service = yodaos.Service({}, api)
+  service[symbol.componentName] = 'foo'
+  application[symbol.registry].service['foo'] = { mod: service }
+
+  application.startService('foo')
+  var actual = application.getService('foo')
+  t.strictEqual(actual, service)
+})
