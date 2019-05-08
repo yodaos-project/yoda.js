73f9633c5f05290a64cd72164ff4ecf7300a068b
morrowindxie <chenfeng.xie@rokid.com>
2019-05-08 20:26:01 +0800

    app/system: support play an alternative voice while speak TTS failed

--- apps/system/app.js
+++ apps/system/app.js
@@ -3,27 +3,43 @@
 var Application = require('@yodaos/application').Application
 var AudioFocus = require('@yodaos/application').AudioFocus
 var SpeechSynthesis = require('@yodaos/speech-synthesis').SpeechSynthesis
+var MediaPlayer = require('@yoda/multimedia').MediaPlayer
 
 var logger = require('logger')('@system')
 var system = require('@yoda/system')
 var battery = require('@yoda/battery')
 var apiInstance
 var synth
+var player = null
 
-function speak (text) {
+function speak (text, altVoice) {
   var focus = new AudioFocus(AudioFocus.Type.TRANSIENT, apiInstance.audioFocus)
   focus.onGain = () => {
     synth.speak(text)
       .on('end', done)
-      .on('error', done)
+      .on('error', doAlt)
       .on('cancel', done)
 
     function done () {
       focus.abandon()
     }
+
+    function doAlt () {
+      if (altVoice != null) {
+        player = new MediaPlayer()
+        player.on('playbackcomplete', done)
+        player.on('error', done)
+        player.start(altVoice)
+      } else {
+        done()
+      }
+    }
   }
   focus.onLoss = () => {
     synth.cancel()
+    if (player != null) {
+      player.stop()
+    }
   }
   focus.request()
 }
@@ -35,7 +51,7 @@ module.exports = (api) => {
     url: function url (urlObj) {
       switch (urlObj.pathname) {
         case '/speak':
-          speak(urlObj.query.text)
+          speak(urlObj.query.text, urlObj.query.alt)
           break
         case '/reboot':
           api.effect.play('system://shutdown.js')
