6cbe72da70d24f480b50ddac0aacb05deb63c5ed
zijiao wang <zijiao.wang@rokid.com>
2019-06-27 16:01:01 +0800

    add lost files

--- client/c/include/yodaos_inner.h
+++ client/c/include/yodaos_inner.h
@@ -1,7 +1,7 @@
 /**
-* @file  yodaos_inner.h  
-* @brief The file just used in sdk, not export.
-*/
+ * @file  yodaos_inner.h
+ * @brief The file just used in sdk, not export.
+ */
 #ifndef YODAOS_INNER_H
 #define YODAOS_INNER_H
 
@@ -15,10 +15,10 @@ extern "C" {
 YODAOS_API_LOCAL void yodaos_send_runtime_ready();
 YODAOS_API_LOCAL void yodaos_send_runtime_active();
 
-YODAOS_API_LOCAL void yodaos_init_flora(const char *moduleName);
-YODAOS_API_LOCAL char *yodaos_read_json_file(char *filepath);
+YODAOS_API_LOCAL void yodaos_init_flora(const char* moduleName);
+YODAOS_API_LOCAL char* yodaos_read_json_file(char* filepath);
 
-YODAOS_API_LOCAL struct yodaos_api_s *yodaos_get_apihd(YODAOS_APINAME api);
+YODAOS_API_LOCAL struct yodaos_api_s* yodaos_get_apihd(YODAOS_APINAME api);
 YODAOS_API_LOCAL int yodaos_check_api_valid(YODAOS_APINAME api);
 
 #ifdef __cplusplus
