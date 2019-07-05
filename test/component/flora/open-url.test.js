yodaos-project/yoda.js

commit 80f6577b800a6b005e3e5e600a9959f087a235fb

runtime: pass url event argument as url string (#787)
To keep Fauna protocol clean and language agnostic, we’d revisit JS related
API and refactor these API. This PR is part of the process. Though we’d keep
actual API untouched to keep API stability at this time, an additional
url parse would be performed at client side.

runtime/component/flora.js
@@ -48,7 +48,7 @@ Flora.prototype.remoteMethods = {
         res.end(0, [ JSON.stringify({ ok: false, message: err.message, stack: err.stack }) ])
       })
   },
-  'yodaos.runtime.open-url-format': function OpenUrl (reqMsg, res) {
+  'yodaos.runtime.open-url-format': function OpenUrlFormat (reqMsg, res) {
     logger.info('open-url with format', reqMsg)
     var urlObj = url.parse(reqMsg[0], true)
     reqMsg.slice(1).forEach(it => {

packages/@yodaos/application/application.js
@@ -49,13 +49,19 @@ function Application (options, api) {
   var registry = application[symbol.registry] = { service: {}, activity: {} }
   classLoader.loadPackage(api, registry)
 
-  ;['created', 'destroyed'].forEach(eve => {
-    api.on(eve, () => {
-      invoke(application, eve)
+  var urlHandler = application[symbol.options]['url']
+  if (typeof urlHandler === 'function') {
+    /**
+     * Compatibility: keep parsing url here to prevent API break change.
+     * Though we should remove it and leave it to apps own choice.
+     */
+    api.on('url', (urlString) => {
+      var url = require('url')
+      urlHandler.call(application, url.parse(urlString))
     })
-  })
+  }
 
-  ;[['url', api], ['broadcast', api.broadcast]].forEach(it => {
+  ;[[ 'created', api ], [ 'destroyed', api ], ['broadcast', api.broadcast]].forEach(it => {
     var eve = it[0]
     var handler = application[symbol.options][eve]
     if (typeof handler !== 'function') {

runtime/app-runtime.js
@@ -256,6 +256,8 @@ AppRuntime.prototype.wakeup = function wakeup (options) {
  */
 AppRuntime.prototype.openUrl = function (url, options) {
   var urlObj = Url.parse(url, true)
+  // ensure to clean up potentially wonky urls.
+  url = Url.format(urlObj)
   if (urlObj.protocol !== 'yoda-app:') {
     logger.info('Url protocol other than yoda-app is not supported now.')
     return Promise.resolve(false)
@@ -268,7 +270,7 @@ AppRuntime.prototype.openUrl = function (url, options) {
 
   return this.component.dispatcher.dispatchAppEvent(
     appId,
-    'url', [ urlObj ],
+    'url', [ url ],
     Object.assign({}, options)
   )
 }

test/@yodaos/application/application.test.js
@@ -1,6 +1,7 @@
 var test = require('tape')
 var EventEmitter = require('events')
 var path = require('path')
+var url = require('url')
 
 var Application = require('@yodaos/application/application')
 
@@ -38,13 +39,13 @@ test('should delegates url events', t => {
   var apiSymbol = Symbol.for('yoda#api')
   global[apiSymbol] = api
 
-  var expectedUrlObj = { href: 'yoda-test://foobar' }
+  var expectedUrlObj = url.parse('yoda-test://foobar')
   Application({
     url: function url (urlObj) {
       t.deepEqual(urlObj, expectedUrlObj)
     }
   })
-  api.emit('url', expectedUrlObj)
+  api.emit('url', expectedUrlObj.href)
 })
 
 test('should delegates methods', t => {

test/@yodaos/application/class-loader.test.js
@@ -13,6 +13,6 @@ test('simple app integration', t => {
   }
 
   require('./fixture/simple-app/app')
-  api.emit('url')
+  api.emit('url', 'yoda-app://foo')
   t.end()
 })

commit 2fa223e60e4fd3b7deaacf7b3cd63cb470f7d549

runtime: query not merged on opening url with format
Fixes issue introduced by #787

runtime/component/flora.js
@@ -57,6 +57,10 @@ Flora.prototype.remoteMethods = {
       }
       urlObj.query[it[0]] = String(it[1])
     })
+    /** Force url format to use un-stringified query object */
+    delete urlObj.search
+    delete urlObj.path
+    delete urlObj.href
     this.runtime.openUrl(urlObj)
       .then(result => {
         res.end(0, [ JSON.stringify({ ok: true, result: result }) ])

test/component/flora/open-url.test.js
@@ -1,31 +1,37 @@
 var test = require('tape')
+var Url = require('url')
+
 var bootstrap = require('./bootstrap')
 var mm = require('../../helper/mock')
 
 test('open url with format', t => {
-  t.plan(3)
+  t.plan(4)
   var suite = bootstrap()
   mm.mockPromise(suite.runtime, 'openUrl', (url) => {
     t.deepEqual(url, {
       protocol: 'yoda-app:',
       slashes: true,
-      auth: null,
-      host: 'foobar',
-      port: null,
+      auth: 'user:secret',
+      host: 'foobar:1234',
+      port: '1234',
       hostname: 'foobar',
-      hash: null,
-      search: '',
+      hash: '#hash',
       query: {
+        foo: 'bar',
         key: '?name',
         number: '123',
         'arbitrary-value': '123,123'
       },
-      pathname: null,
-      path: null,
-      href: 'yoda-app://foobar'
+      pathname: '/pathname'
     })
+    t.strictEqual(Url.format(url), 'yoda-app://user:secret@foobar:1234/pathname?foo=bar&key=%3Fname&number=123&arbitrary-value=123%2C123#hash')
   })
-  suite.floraCall('yodaos.runtime.open-url-format', ['yoda-app://foobar', ['key', '?name'], ['number', 123], ['arbitrary-value', ['123', 123]]])
+  suite.floraCall('yodaos.runtime.open-url-format', [
+    'yoda-app://user:secret@foobar:1234/pathname?foo=bar#hash',
+    ['key', '?name'],
+    ['number', 123],
+    ['arbitrary-value', ['123', 123]]
+  ])
     .then(res => {
       t.strictEqual(res.code, 0)
       t.strictEqual(res.msg[0], '{"ok":true}')