48edf62a6582807814c2a73fb2aa3e8c79b1357d
legendecas <legendecas@gmail.com>
2019-08-23 15:04:00 +0800

    src: move apps to separate repos (#864)

--- test/helper/index.js
+++ test/helper/index.js
@@ -2,13 +2,11 @@ var path = require('path')
 
 module.exports.paths = {
   runtime: '/usr/yoda',
-  apps: '/opt/apps',
   fixture: path.join(__dirname, '..', 'fixture')
 }
 
 if (process.env.YODA_RUN_MODE === 'host') {
   Object.assign(module.exports.paths, {
-    runtime: path.join(__dirname, '..', '..', 'runtime'),
-    apps: path.join(__dirname, '..', '..', 'apps')
+    runtime: path.join(__dirname, '..', '..', 'runtime')
   })
 }
